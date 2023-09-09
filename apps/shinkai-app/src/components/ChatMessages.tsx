1ad0328e1cf124792dfc4ab15a8cb1b21e3d3595
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2023-09-09 08:51:26 +0800

    final fixes

--- apps/shinkai-app/src/components/ChatMessages.tsx
+++ apps/shinkai-app/src/components/ChatMessages.tsx
@@ -1,20 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getLastMessagesFromInbox, getLastUnreadMessagesFromInbox } from "@shinkai/shinkai-message-ts/api/methods";
-import { ShinkaiMessage } from "@shinkai/shinkai-message-ts/models";
-import { IonList, IonItem, IonButton } from "@ionic/react";
-import Avatar from "../components/ui/Avatar";
-import { cn } from "../theme/lib/utils";
-import { IonContentCustom } from "./ui/Layout";
-import { calculateMessageHash } from "@shinkai/shinkai-message-ts/utils/shinkai_message_handler";
-import { RootState } from "../store";
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  getLastMessagesFromInbox,
+  getLastUnreadMessagesFromInbox,
+} from '@shinkai/shinkai-message-ts/api/methods';
+import { ShinkaiMessage } from '@shinkai/shinkai-message-ts/models';
+import { IonList, IonItem, IonButton } from '@ionic/react';
+import Avatar from '../components/ui/Avatar';
+import { cn } from '../theme/lib/utils';
+import { IonContentCustom } from './ui/Layout';
+import { calculateMessageHash } from '@shinkai/shinkai-message-ts/utils/shinkai_message_handler';
+import { RootState } from '../store';
+import { receiveLastMessagesFromInbox } from '../store/actions';
 
 interface ChatMessagesProps {
   deserializedId: string;
 }
 
 const ChatMessages: React.FC<ChatMessagesProps> = ({ deserializedId }) => {
-  console.log("Loading ChatMessages.tsx");
+  console.log('Loading ChatMessages.tsx');
   const dispatch = useDispatch();
   const setupDetailsState = useSelector(
     (state: RootState) => state.setupDetails
@@ -24,41 +28,57 @@ const ChatMessages: React.FC<ChatMessagesProps> = ({ deserializedId }) => {
   );
 
   const [lastKey, setLastKey] = useState<string | undefined>(undefined);
-  const [mostRecentKey, setMostRecentKey] = useState<string | undefined>(undefined);
+  const [mostRecentKey, setMostRecentKey] = useState<string | undefined>(
+    undefined
+  );
   const [prevMessagesLength, setPrevMessagesLength] = useState(0);
   const [hasMoreMessages, setHasMoreMessages] = useState(true);
   const [messages, setMessages] = useState<ShinkaiMessage[]>([]);
 
   useEffect(() => {
-    console.log("deserializedId:", deserializedId);
-    dispatch(
-      getLastMessagesFromInbox(deserializedId, 10, lastKey, setupDetailsState)
-    );
-  }, [dispatch, setupDetailsState]);
+    console.log('deserializedId:', deserializedId);
+    getLastMessagesFromInbox(
+      deserializedId,
+      10,
+      lastKey,
+      setupDetailsState
+    ).then((messages) => {
+      dispatch(receiveLastMessagesFromInbox(deserializedId, messages));
+    });
+  }, [dispatch, setupDetailsState, deserializedId, lastKey]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const lastMessage = reduxMessages[reduxMessages.length - 1];
-      const hashKey = calculateMessageHash(lastMessage);
-      dispatch(
-        getLastUnreadMessagesFromInbox(deserializedId, 10, mostRecentKey, setupDetailsState)
-      );
+      getLastUnreadMessagesFromInbox(
+        deserializedId,
+        10,
+        mostRecentKey,
+        setupDetailsState
+      ).then((messages) => {
+        dispatch(receiveLastMessagesFromInbox(deserializedId, messages));
+      });
     }, 5000); // 2000 milliseconds = 2 seconds
     return () => clearInterval(interval);
-  }, [dispatch, deserializedId, mostRecentKey, setupDetailsState, reduxMessages]);
+  }, [
+    dispatch,
+    deserializedId,
+    mostRecentKey,
+    setupDetailsState,
+  ]);
 
   useEffect(() => {
     if (reduxMessages && reduxMessages.length > 0) {
       // console.log("Redux Messages:", reduxMessages);
       const lastMessage = reduxMessages[reduxMessages.length - 1];
-      console.log("Last Message:", lastMessage);
+      console.log('Last Message:', lastMessage);
       const timeKey = lastMessage.external_metadata.scheduled_time;
       const hashKey = calculateMessageHash(lastMessage);
       const lastMessageKey = `${timeKey}:::${hashKey}`;
       setLastKey(lastMessageKey);
 
       const mostRecentMessage = reduxMessages[0];
-      const mostRecentTimeKey = mostRecentMessage.external_metadata.scheduled_time;
+      const mostRecentTimeKey =
+        mostRecentMessage.external_metadata.scheduled_time;
       const mostRecentHashKey = calculateMessageHash(mostRecentMessage);
       const mostRecentMessageKey = `${mostRecentTimeKey}:::${mostRecentHashKey}`;
       setMostRecentKey(mostRecentMessageKey);
@@ -70,12 +90,12 @@ const ChatMessages: React.FC<ChatMessagesProps> = ({ deserializedId }) => {
       }
       setPrevMessagesLength(reduxMessages.length);
     }
-  }, [reduxMessages]);
+  }, [reduxMessages, prevMessagesLength]);
 
   const extractContent = (messageBody: any) => {
     // TODO: extend it so it can be re-used by JobChat or normal Chat
-    if (messageBody && "unencrypted" in messageBody) {
-      if ("unencrypted" in messageBody.unencrypted.message_data) {
+    if (messageBody && 'unencrypted' in messageBody) {
+      if ('unencrypted' in messageBody.unencrypted.message_data) {
         return JSON.parse(
           messageBody.unencrypted.message_data.unencrypted.message_raw_content
         ).content;
@@ -87,7 +107,7 @@ const ChatMessages: React.FC<ChatMessagesProps> = ({ deserializedId }) => {
     } else if (messageBody?.encrypted) {
       return JSON.parse(messageBody.encrypted.content).content;
     }
-    return "";
+    return '';
   };
 
   return (
@@ -101,7 +121,7 @@ const ChatMessages: React.FC<ChatMessagesProps> = ({ deserializedId }) => {
                   deserializedId,
                   10,
                   lastKey,
-                  setupDetailsState,
+                  setupDetailsState
                   // true
                 )
               )
@@ -112,58 +132,56 @@ const ChatMessages: React.FC<ChatMessagesProps> = ({ deserializedId }) => {
         )}
         <IonList class="ion-list-chat p-0 divide-y divide-slate-200 dark:divide-slate-500/50 md:rounded=[1.25rem]  ">
           {messages &&
-            messages
-              .slice()
-              .map((message, index) => {
-                const { shinkai_identity, profile, registration_name } =
-                  setupDetailsState;
+            messages.slice().map((message, index) => {
+              const { shinkai_identity, profile, registration_name } =
+                setupDetailsState;
 
-                const localIdentity = `${profile}/device/${registration_name}`;
-                // console.log("Message:", message);
-                let isLocalMessage = false;
-                if (message.body && "unencrypted" in message.body) {
-                  isLocalMessage =
-                    message.body.unencrypted.internal_metadata
-                      .sender_subidentity === localIdentity;
-                }
+              const localIdentity = `${profile}/device/${registration_name}`;
+              // console.log("Message:", message);
+              let isLocalMessage = false;
+              if (message.body && 'unencrypted' in message.body) {
+                isLocalMessage =
+                  message.body.unencrypted.internal_metadata
+                    .sender_subidentity === localIdentity;
+              }
 
-                return (
-                  <IonItem
-                    key={index}
-                    lines="none"
-                    className={cn(
-                      "ion-item-chat relative w-full shadow",
-                      isLocalMessage && "isLocalMessage"
+              return (
+                <IonItem
+                  key={index}
+                  lines="none"
+                  className={cn(
+                    'ion-item-chat relative w-full shadow',
+                    isLocalMessage && 'isLocalMessage'
+                  )}
+                >
+                  <div className="px-2 py-4 flex gap-4 pb-10 w-full">
+                    <Avatar
+                      className="shrink-0 mr-4"
+                      url={
+                        isLocalMessage
+                          ? 'https://ui-avatars.com/api/?name=Me&background=FE6162&color=fff'
+                          : 'https://ui-avatars.com/api/?name=O&background=363636&color=fff'
+                      }
+                    />
+
+                    <p>{extractContent(message.body)}</p>
+                    {message?.external_metadata?.scheduled_time && (
+                      <span className="absolute bottom-[5px] right-5 text-muted text-sm">
+                        {new Date(
+                          message.external_metadata.scheduled_time
+                        ).toLocaleString(undefined, {
+                          year: 'numeric',
+                          month: 'long',
+                          day: 'numeric',
+                          hour: '2-digit',
+                          minute: '2-digit',
+                        })}
+                      </span>
                     )}
-                  >
-                    <div className="px-2 py-4 flex gap-4 pb-10 w-full">
-                      <Avatar
-                        className="shrink-0 mr-4"
-                        url={
-                          isLocalMessage
-                            ? "https://ui-avatars.com/api/?name=Me&background=FE6162&color=fff"
-                            : "https://ui-avatars.com/api/?name=O&background=363636&color=fff"
-                        }
-                      />
-
-                      <p>{extractContent(message.body)}</p>
-                      {message?.external_metadata?.scheduled_time && (
-                        <span className="absolute bottom-[5px] right-5 text-muted text-sm">
-                          {new Date(
-                            message.external_metadata.scheduled_time
-                          ).toLocaleString(undefined, {
-                            year: "numeric",
-                            month: "long",
-                            day: "numeric",
-                            hour: "2-digit",
-                            minute: "2-digit",
-                          })}
-                        </span>
-                      )}
-                    </div>
-                  </IonItem>
-                );
-              })}
+                  </div>
+                </IonItem>
+              );
+            })}
         </IonList>
       </div>
     </IonContentCustom>
--- apps/shinkai-app/src/pages/JobChat.tsx
+++ apps/shinkai-app/src/pages/JobChat.tsx
@@ -5,30 +5,25 @@ import {
   IonPage,
   IonTextarea,
   IonTitle,
-} from "@ionic/react";
-import { useParams } from "react-router-dom";
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import {
-  sendMessageToJob,
-} from "@shinkai/shinkai-message-ts/api";
-import { useSetup } from "../hooks/usetSetup";
+} from '@ionic/react';
+import { useParams } from 'react-router-dom';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { sendMessageToJob } from '@shinkai/shinkai-message-ts/api';
+import { useSetup } from '../hooks/usetSetup';
 import {
   extractJobIdFromInbox,
   getOtherPersonIdentity,
-} from "../utils/inbox_name_handler";
-import { cn } from "../theme/lib/utils";
-import { send } from "ionicons/icons";
-import "./Chat.css";
-import {
-  IonFooterCustom,
-  IonHeaderCustom,
-} from "../components/ui/Layout";
-import ChatMessages from "../components/ChatMessages";
-import { RootState } from "../store";
+} from '../utils/inbox_name_handler';
+import { cn } from '../theme/lib/utils';
+import { send } from 'ionicons/icons';
+import './Chat.css';
+import { IonFooterCustom, IonHeaderCustom } from '../components/ui/Layout';
+import ChatMessages from '../components/ChatMessages';
+import { RootState } from '../store';
 
 const JobChat: React.FC = () => {
-  console.log("Loading JobChat.tsx");
+  console.log('Loading JobChat.tsx');
   useSetup();
 
   const dispatch = useDispatch();
@@ -39,8 +34,8 @@ const JobChat: React.FC = () => {
   const { shinkai_identity, profile } = setupDetailsState;
 
   const { id } = useParams<{ id: string }>();
-  const deserializedId = decodeURIComponent(id).replace(/~/g, ".");
-  const [inputMessage, setInputMessage] = useState("");
+  const deserializedId = decodeURIComponent(id).replace(/~/g, '.');
+  const [inputMessage, setInputMessage] = useState('');
   const otherPersonIdentity = getOtherPersonIdentity(
     deserializedId,
     setupDetailsState.shinkai_identity
@@ -48,23 +43,29 @@ const JobChat: React.FC = () => {
 
   const sendMessage = useCallback(async () => {
     const sender = `${shinkai_identity}/${profile}`;
-    console.log("Sending message: ", inputMessage);
-    console.log("Sender:", sender);
+    console.log('Sending message: ', inputMessage);
+    console.log('Sender:', sender);
 
     const message_to_send = inputMessage;
-    setInputMessage("");
-    const result = await dispatch(
-      sendMessageToJob(
-        extractJobIdFromInbox(deserializedId.toString()),
-        message_to_send,
-        sender,
-        shinkai_identity,
-        "",
-        setupDetailsState
-      )
-    );
-
-  }, [inputMessage, dispatch, setupDetailsState, shinkai_identity, deserializedId]);
+    setInputMessage('');
+    sendMessageToJob(
+      extractJobIdFromInbox(deserializedId.toString()),
+      message_to_send,
+      sender,
+      shinkai_identity,
+      '',
+      setupDetailsState
+    ).then((message) => {
+      dispatch({ type: 'SEND_MESSAGE_SUCCESS', payload: message });
+    });
+  }, [
+    inputMessage,
+    dispatch,
+    setupDetailsState,
+    shinkai_identity,
+    deserializedId,
+    profile
+  ]);
 
   return (
     <IonPage className="bg-slate-900">
@@ -79,17 +80,15 @@ const JobChat: React.FC = () => {
           {/*<Avatar className="shrink-0" />*/}
         </div>
       </IonHeaderCustom>
-      <ChatMessages
-        deserializedId={deserializedId}
-      />
+      <ChatMessages deserializedId={deserializedId} />
       <IonFooterCustom>
         <form
           className={
-            "flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative"
+            'flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative'
           }
           onSubmit={(e) => {
             e.preventDefault();
-            if (inputMessage.trim() !== "") {
+            if (inputMessage.trim() !== '') {
               sendMessage();
             }
           }}
@@ -113,10 +112,10 @@ const JobChat: React.FC = () => {
               onClick={sendMessage}
               aria-label="Send Message"
               className={cn(
-                "absolute z-10 p-3 rounded-md text-gray-500 bottom-[1px] right-1",
-                "md:bottom-2.5 md:right-2",
-                "hover:bg-gray-100 disabled:hover:bg-transparent",
-                "dark:text-white dark:hover:text-gray-100 dark:hover:bg-gray-700 dark:disabled:hover:bg-transparent"
+                'absolute z-10 p-3 rounded-md text-gray-500 bottom-[1px] right-1',
+                'md:bottom-2.5 md:right-2',
+                'hover:bg-gray-100 disabled:hover:bg-transparent',
+                'dark:text-white dark:hover:text-gray-100 dark:hover:bg-gray-700 dark:disabled:hover:bg-transparent'
               )}
             >
               <IonIcon size="" icon={send} />
