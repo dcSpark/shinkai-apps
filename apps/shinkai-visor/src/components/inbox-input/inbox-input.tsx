c0e17cffd58918c7ae8903c24f92f58509a95dc7
Alfredo Gallardo <agallardol@outlook.com>
2023-10-03 11:06:25 -0500

    - refactor: removed antd from inbox input

--- apps/shinkai-visor/src/components/inbox-input/inbox-input.tsx
+++ apps/shinkai-visor/src/components/inbox-input/inbox-input.tsx
@@ -1,57 +1,83 @@
-import { SendOutlined } from '@ant-design/icons';
-import { Button, Form, Input, Space } from 'antd';
-import { useEffect, useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Loader2, Send } from 'lucide-react';
+import { useForm } from 'react-hook-form';
 import { useIntl } from 'react-intl';
+import { z } from 'zod';
 
-type InboxInputFieldType = {
-  message: string;
-};
+import { Button } from '../ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '../ui/form';
+import { Input } from '../ui/input';
+
+const formSchema = z.object({
+  message: z.string().nonempty(),
+});
+
+type InboxInputFieldType = z.infer<typeof formSchema>;
+
 type InboxInputProps = {
   onSubmit: (value: string) => void;
   onChange?: (value: string) => void;
   disabled?: boolean;
   loading?: boolean;
 };
+
 export const InboxInput = (props: InboxInputProps) => {
-  const [form] = Form.useForm<InboxInputFieldType>();
+  const form = useForm<InboxInputFieldType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: '',
+    },
+  });
   const intl = useIntl();
-  const [submittable, setSubmittable] = useState(false);
-  const currentFormValue = Form.useWatch([], form);
-  const submit = () => {
-    const value = currentFormValue?.message?.trim();
+  const submit = (values: InboxInputFieldType) => {
+    const value = values?.message?.trim();
     if (value) {
       props.onSubmit(value);
-      form.setFieldValue('message', '');
+      form.reset({ message: '' });
     }
   };
-  useEffect(() => {
-    setSubmittable(!!currentFormValue?.message?.trim());
-  }, [currentFormValue]);
   return (
-    <div className="w-full flex flex-row place-content-between space-x-3">
-      <Space.Compact block>
-        <Form
-          autoComplete="off"
-          className="grow"
-          disabled={props.disabled}
-          form={form}
-          onSubmitCapture={() => submit()}
+    <Form {...form}>
+      <form
+        className="p-1 flex flex-row space-x-2 justify-between"
+        onSubmit={form.handleSubmit(submit)}
+      >
+        <div className="grow flex flex-col space-y-2">
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder={intl.formatMessage({
+                      id: 'registration-name',
+                    })}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button
+          className="grow-0"
+          disabled={!form.formState.isValid || props.disabled}
         >
-          <Form.Item<InboxInputFieldType> name="message" required={false}>
-            <Input placeholder={intl.formatMessage({ id: 'message.one' })} />
-          </Form.Item>
-        </Form>
-        <Form.Item>
-          <Button
-            disabled={props.disabled || !submittable}
-            htmlType="submit"
-            icon={<SendOutlined />}
-            loading={props.loading}
-            onClick={() => submit()}
-            type="primary"
-          ></Button>
-        </Form.Item>
-      </Space.Compact>
-    </div>
+          {props.loading ? (
+            <Loader2 className="h-4 w-4 animate-spin" />
+          ) : (
+            <Send className="w-4 h-4" />
+          )}
+        </Button>
+      </form>
+    </Form>
   );
 };
