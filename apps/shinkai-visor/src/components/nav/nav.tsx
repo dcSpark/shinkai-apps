9279b161a468c210017618f5b5c09b9d5ca3d5b7
Alfredo Gallardo <agallardol@outlook.com>
2023-10-03 10:29:38 -0500

    - refactor: removed atnd from nav

--- apps/shinkai-visor/src/components/nav/nav.tsx
+++ apps/shinkai-visor/src/components/nav/nav.tsx
@@ -1,49 +1,51 @@
 import './nav.css';
 
-import {
-  ArrowLeftOutlined,
-  CloseOutlined,
-  MenuOutlined,
-} from '@ant-design/icons';
-import { Button } from 'antd';
+import { ArrowLeft, Menu, X } from 'lucide-react';
 import { useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
 import visorLogo from '../../assets/icons/visor.svg';
 import { srcUrlResolver } from '../../helpers/src-url-resolver';
 import { NavMenu } from '../nav-menu/nav-menu';
+import { Button } from '../ui/button';
 
 export default function NavBar() {
   const history = useHistory();
   const location = useLocation();
   const [isMenuOpened, setMenuOpened] = useState(false);
-  const isRootPage = ['/welcome', '/inboxes', '/agents', '/jobs'].includes(location.pathname);
+  const isRootPage = ['/welcome', '/inboxes', '/agents', '/jobs'].includes(
+    location.pathname,
+  );
 
   function goBack() {
     history.goBack();
   }
 
   return (
-    <nav
-      className="flex flex-col bg-gray-100 shadow-lg p-5 rounded-lg space-y-6"
-      
-    >
+    <nav className="flex flex-col bg-gray-100 shadow-lg p-5 rounded-lg space-y-6">
       <div className="flex flex-row place-items-center justify-between">
-        <div className="flex-none w-8">
+        <div className="flex-none">
           {!isRootPage && (
-            <Button
-              className="shinkai-borderless-button"
-              icon={<ArrowLeftOutlined />}
-              onClick={() => goBack()}
-            ></Button>
+            <Button onClick={() => goBack()}>
+              <ArrowLeft className="h-4 w-4" />
+            </Button>
           )}
         </div>
-        <img alt="shinkai-app-logo" className="h-full" src={srcUrlResolver(visorLogo)} />
+        <img
+          alt="shinkai-app-logo"
+          className="h-full"
+          src={srcUrlResolver(visorLogo)}
+        />
         <Button
-          className="flex-none w-8 shinkai-borderless-button"
-          icon={!isMenuOpened ? <MenuOutlined /> : <CloseOutlined />}
+          className="flex-none"
           onClick={() => setMenuOpened(!isMenuOpened)}
-        ></Button>
+        >
+          {!isMenuOpened ? (
+            <Menu className="h-4 w-4" />
+          ) : (
+            <X className="h-4 w-4" />
+          )}
+        </Button>
       </div>
       {isMenuOpened && <NavMenu />}
     </nav>
