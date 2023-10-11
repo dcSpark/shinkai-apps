023fa4e530355fd8b8efe5b86992dac8e1159db4
Alfredo Gallardo <agallardol@outlook.com>
2023-10-11 11:34:18 -0500

    - fix: removed unused code

--- apps/shinkai-visor/src/service-worker/communication/service-worker-message-type.ts
+++ apps/shinkai-visor/src/service-worker/communication/service-worker-message-type.ts
@@ -2,5 +2,4 @@ export enum ServiceWorkerMessageType {
   ContentScript = 'content-script',
   SendToAgent = 'send-to-agent',
   SendPageToAgent = 'send-page-to-agent',
-  OffscreenMessageType = 'offscreen',
 }
