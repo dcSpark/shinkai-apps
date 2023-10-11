8bfa236c1f86f983fe43eecab2f6035e2b281a68
Alfredo Gallardo <agallardol@outlook.com>
2023-10-11 08:39:47 -0500

    - fix: removed unused resources

--- apps/shinkai-visor/src/store/auth/auth-types.ts
+++
@@ -1,3 +0,0 @@
-export interface AuthStore {
-  status: 'authenticated' | 'unauthenticated',
-}
--- apps/shinkai-visor/src/store/jobs/jobs-types.ts
+++
@@ -1,9 +0,0 @@
-import { AsyncData } from "../helpers/async-data";
-
-export type Job = {
-  id: string;
-};
-
-export interface JobsState {
-  create: AsyncData<{ job: Job }>,
-}
