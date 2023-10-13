06e6cfca3ca5e9f8fc8faf982decde0881950a19
paulclindo <paulclindo@gmail.com>
2023-10-12 21:42:19 -0500

    refactor agents

--- apps/shinkai-app/src/components/ui/Button.tsx
+++ apps/shinkai-app/src/components/ui/Button.tsx
@@ -1,34 +1,41 @@
-import "./Button.css";
+import './Button.css';
 
-import { IonButton } from "@ionic/react";
-import React from "react";
+import { IonButton } from '@ionic/react';
+import { Loader } from 'lucide-react';
+import React from 'react';
 
-import { cn } from "../../theme/lib/utils";
+import { cn } from '../../theme/lib/utils';
 
 export default function Button({
   onClick,
   disabled,
   children,
   className,
-  variant = "primary",
+  variant = 'primary',
+  type = 'button',
+  isLoading,
 }: {
-  onClick: () => void;
+  onClick?: () => void;
   disabled?: boolean;
   children: React.ReactNode;
   className?: string;
-  variant?: "primary" | "secondary" | "tertiary";
+  variant?: 'primary' | 'secondary' | 'tertiary';
+  type?: 'button' | 'submit' | 'reset';
+  isLoading?: boolean;
 }) {
   return (
     <IonButton
       className={cn(
-        "ion-button-custom w-full",
-        "variant-" + variant,
-        className,
+        'ion-button-custom w-full',
+        'variant-' + variant,
+        className
       )}
-      disabled={disabled}
+      disabled={disabled || isLoading}
       onClick={onClick}
-      shape={"round"}
+      shape={'round'}
+      type={type}
     >
+      {isLoading ? <Loader className="h-4 w-4 animate-spin mr-2" /> : null}
       {children}
     </IonButton>
   );
