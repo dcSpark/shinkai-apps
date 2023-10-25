0a2d3418f9083603e2d1228d6b88bb7e85a090d8
Alfredo Gallardo <agallardol@outlook.com>
2023-10-25 15:32:56 -0300

    - feature: added cicd for chrome

--- apps/shinkai-visor/dynamic-manifest.ts
+++ apps/shinkai-visor/dynamic-manifest.ts
@@ -4,16 +4,26 @@ import baseManifestJson from './public/manifest.json';
 
 
 const getVersion = () => {
-  const version = process.env.VERSION || baseManifestJson.version || '0.0.0.0';
+  const version = process.env.VERSION || baseManifestJson.version || '0.0.0.1';
   const [major, minor, patch, label = '0'] = version
     .replace(/[^\d.-]+/g, '')
     .split(/[.-]/);
   return `${major}.${minor}.${patch}.${label}`;
 };
 
+const getName = () => {
+  return `${process.env.NAME_PREFIX}${baseManifestJson.name}`;
+}
+
+const getDescription = () => {
+  return `${process.env.DESCRIPTION_PREFIX}${baseManifestJson.description}`;
+}
+
 export const dynamicManifest = defineManifest((env) => {
   return {
     ...baseManifestJson,
     version: getVersion(),
+    name: getName(),
+    description: getDescription(),
   };
 });
