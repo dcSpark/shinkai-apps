55d5c2101bc765c56738e17568fd97a5e3b14fb6
Alfredo Gallardo <agallardol@outlook.com>
2023-11-02 13:01:53 -0300

    - improve: change logout with delete conncection

--- apps/shinkai-visor/src/components/nav/nav.tsx
+++ apps/shinkai-visor/src/components/nav/nav.tsx
@@ -218,7 +218,7 @@ export default function NavBar() {
               >
                 <LogOut className="mr-2 h-4 w-4" />
                 <span>
-                  <FormattedMessage id="logout" />
+                  <FormattedMessage id="delete-connection" />
                 </span>
               </DropdownMenuItem>
             </DropdownMenuContent>
