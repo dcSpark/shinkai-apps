59a3c2095ffcef649547f7c5f6367b220c6e577b
Alfredo Gallardo <agallardol@outlook.com>
2023-11-07 10:23:31 -0300

    - feature: File input in create job 7-shinkai roadmap internal (#70)
    * - feature: added file input to job creation
    
    * - fix: file input state

---
+++ apps/shinkai-visor/src/components/file-input/file-input.tsx
@@ -0,0 +1,154 @@
+import { ExternalLink, Trash, Upload } from 'lucide-react';
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import { cn } from '../../helpers/cn-utils';
+import { getFileExt } from '../../helpers/file-name-utils';
+import { FileList } from '../file-list/file-list';
+
+export type FileInputProps = {
+  value: File[];
+  extensions?: string[];
+  openExternal?: boolean;
+  multiple?: boolean;
+  onFilesAdded?: (files: File[]) => void;
+  onFileRemoved?: (file: File, index: number) => void;
+  onValueChange?: (files: File[]) => void;
+};
+
+export const FileInput = ({
+  value,
+  extensions,
+  multiple,
+  onFilesAdded: onFileAdded,
+  onFileRemoved,
+  onValueChange: onValueChanged,
+}: FileInputProps) => {
+  const files = useRef<File[]>([]);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [, updateState] = useState<number>(0);
+  const forceUpdate = useCallback(() => updateState(Date.now()), []);
+  const onAddFiles = (event: ChangeEvent<HTMLInputElement>): void => {
+    const filesAdded = Array.from(event?.target?.files || []);
+    files.current = [...files.current, ...filesAdded];
+    if (typeof onFileAdded === 'function') {
+      onFileAdded(filesAdded);
+    }
+    if (typeof onValueChanged === 'function') {
+      onValueChanged(files.current);
+    }
+  };
+  const removeFile = (file: File, index: number): void => {
+    // Warn: We are mutating files
+    files.current.splice(index, 1);
+    if (typeof onFileRemoved === 'function') {
+      onFileRemoved(file, index);
+    }
+    if (typeof onValueChanged === 'function') {
+      onValueChanged(files.current);
+    }
+  };
+  const openFile = (file: File): void => {
+    const fileURL = window.URL.createObjectURL(file);
+    window.open(fileURL, '_blank');
+    URL.revokeObjectURL(fileURL);
+  };
+  const isAccepted = (file: File): boolean => {
+    return !!extensions?.includes(`.${getFileExt(file.name)}`);
+  };
+  const onDropFiles = (event: React.DragEvent<HTMLLabelElement>) => {
+    event.preventDefault();
+    let draggedFiles: File[] = [];
+    if (event.dataTransfer.items) {
+      Array.from(event.dataTransfer.items || []).forEach((item, i) => {
+        if (item.kind === 'file') {
+          const file = item.getAsFile();
+          if (!file) {
+            return;
+          }
+          draggedFiles.push(file);
+        }
+      });
+    } else {
+      draggedFiles.push(...Array.from(event.dataTransfer.files || []));
+    }
+    draggedFiles = draggedFiles.filter((file) => isAccepted(file));
+    files.current = [...files.current, ...draggedFiles];
+    if (typeof onFileAdded === 'function') {
+      onFileAdded(draggedFiles);
+    }
+    if (typeof onValueChanged === 'function') {
+      onValueChanged(files.current);
+    }
+  };
+  const onDragOver = (event: React.DragEvent<HTMLLabelElement>) => {
+    event.preventDefault();
+  };
+  useEffect(() => {
+    files.current = value;
+    forceUpdate();
+  }, [value]);
+  return (
+    <div className="flex flex-col space-y-1">
+      <div className="flex items-center justify-center">
+        <label
+          className={cn(
+            'flex flex-col items-center justify-center w-full h-[80px] p-2 border-2 border-dashed rounded-lg cursor-pointer bg-secondary-600',
+            isDragging && 'border-solid'
+          )}
+          htmlFor="dropzone-file"
+          onDragEnter={() => setIsDragging(true)}
+          onDragLeave={() => setIsDragging(false)}
+          onDragOver={(event) => onDragOver(event)}
+          onDrop={(event) => onDropFiles(event)}
+        >
+          <div
+            className={cn(
+              'flex flex-col items-center justify-center space-y-1 w-full',
+              isDragging && 'pointer-events-none'
+            )}
+          >
+            <div>
+              <Upload
+                className={cn('w-4 h-4', isDragging && 'animate-pulse')}
+              />
+            </div>
+            <p className="text-sm text-gray-500">
+              <FormattedMessage id="click-to-upload" />
+            </p>
+            <p className="text-xs text-gray-500 truncate w-full">
+              {extensions?.join(' | ')}
+            </p>
+          </div>
+          <input
+            accept={extensions?.join(',')}
+            alt="files input"
+            className="hidden"
+            id="dropzone-file"
+            multiple={multiple}
+            onChange={(event) => onAddFiles(event)}
+            type="file"
+          />
+        </label>
+      </div>
+
+      {!!files?.current.length && (
+        <FileList
+          actions={[
+            {
+              render: (
+                <ExternalLink className="w-4 h-4 cursor-pointer"></ExternalLink>
+              ),
+              onClick: (file) => openFile(file),
+            },
+            {
+              render: <Trash className="w-4 h-4 cursor-pointer"></Trash>,
+              onClick: (file, index) => removeFile(file, index),
+            },
+          ]}
+          files={files.current}
+        ></FileList>
+      )}
+    </div>
+  );
+};
--- apps/shinkai-visor/src/components/file-list/file-list.tsx
+++ apps/shinkai-visor/src/components/file-list/file-list.tsx
@@ -1,46 +1,69 @@
-import { partial } from "filesize";
-import { ExternalLink, Paperclip } from 'lucide-react';
+import { partial } from 'filesize';
+import { AnimatePresence, motion } from 'framer-motion';
+import { Paperclip } from 'lucide-react';
+import { ReactNode } from 'react';
 
 import { getFileExt, getFileName } from '../../helpers/file-name-utils';
 
-export const FileList = ({ files }: { files: File[] }) => {
-  const openFile = (file: File): void => {
-    const fileURL = window.URL.createObjectURL(file);
-    window.open(fileURL, '_blank');
-    URL.revokeObjectURL(fileURL);
+export type FileListProps = {
+  files: File[];
+  actions: {
+    render: ReactNode;
+    onClick?: (file: File, index: number) => void;
+  }[];
+};
+export const FileList = ({ files, actions }: FileListProps) => {
+  const size = partial({ standard: 'jedec' });
+  const animations = {
+    initial: { scale: 0, opacity: 0 },
+    animate: { scale: 1, opacity: 1 },
+    exit: { scale: 0, opacity: 0 },
   };
-  const size = partial({standard: "jedec"});
-
   return (
     <ul className="divide-y divide-gray-100 rounded-md border border-gray-200">
-      {files?.map((file, index) => (
-        <li
-          className="flex items-center justify-between p-2 text-sm leading-6"
-          key={index}
-        >
-          <div className="flex w-0 flex-1 space-x-2 items-center">
-            <Paperclip className="w-4 h-4"></Paperclip>
-            <div className="ml-4 flex min-w-0 flex-1 gap-2 justify-between">
-              <div className="flex flex-row overflow-hidden">
-                <span className="truncate font-medium">
-                  {getFileName(decodeURIComponent(file.name))}
-                </span>
-                <span className="font-medium">.</span>
-                <span className="font-medium">
-                  {getFileExt(decodeURIComponent(file.name))}
+      <AnimatePresence>
+        {files?.map((file, index) => (
+          <motion.li
+            {...animations}
+            className="flex items-center justify-between p-2 text-sm leading-6"
+            key={index}
+          >
+            <div className="flex w-0 flex-1 space-x-2 items-center">
+              <Paperclip className="w-4 h-4"></Paperclip>
+              <div className="ml-4 flex min-w-0 flex-1 gap-2 justify-between">
+                <div className="flex flex-row overflow-hidden">
+                  <span className="truncate font-medium">
+                    {getFileName(decodeURIComponent(file.name))}
+                  </span>
+                  <span className="font-medium">.</span>
+                  <span className="font-medium">
+                    {getFileExt(decodeURIComponent(file.name))}
+                  </span>
+                </div>
+                <span className="flex-shrink-0 text-gray-400">
+                  {size(file.size)}
                 </span>
               </div>
-              <span className="flex-shrink-0 text-gray-400">
-                {size(file.size)}
-              </span>
+              <div className="flex flex-row space-x-1">
+                {actions?.map((action, actionIndex) => {
+                  return (
+                    <div
+                      key={actionIndex}
+                      onClick={() => {
+                        if (typeof action.onClick === 'function') {
+                          action.onClick(file, index);
+                        }
+                      }}
+                    >
+                      {action.render}
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-            <ExternalLink
-              className="w-4 h-4 cursor-pointer"
-              onClick={() => openFile(file)}
-            ></ExternalLink>
-          </div>
-        </li>
-      ))}
+          </motion.li>
+        ))}
+      </AnimatePresence>
     </ul>
   );
 };
