7bff06cac53858278b25f796354a1d03807f1258
paulclindo <paulclindo@gmail.com>
2023-11-28 13:30:27 -0500

    add privacy and terms

--- apps/shinkai-visor/src/components/welcome/welcome.tsx
+++ apps/shinkai-visor/src/components/welcome/welcome.tsx
@@ -8,7 +8,7 @@ import { srcUrlResolver } from '../../helpers/src-url-resolver';
 export default function Welcome() {
   const history = useHistory();
   return (
-    <div className="flex h-full flex-col justify-between">
+    <div className="flex h-full flex-col justify-between gap-3">
       <div>
         <div className="grid place-content-center ">
           <img
@@ -18,7 +18,7 @@ export default function Welcome() {
             src={srcUrlResolver(logo)}
           />
         </div>
-        <div className="-ml-6 -mr-6 pb-6 pt-3">
+        <div className="-ml-6 -mr-6 pt-1">
           <img
             alt="shinkai logo"
             className="animate-spin-slow w-full"
@@ -35,6 +35,28 @@ export default function Welcome() {
       >
         <FormattedMessage id="setup" />
       </Button>
+      <p className="text-center">
+        <span className="block">By continuing, you agree to our</span>
+        <span>
+          <a
+            className={'px-1 text-blue-400 underline'}
+            href={'https://www.shinkai.com/terms-of-service'}
+            rel="noreferrer"
+            target={'_blank'}
+          >
+            Terms of Service
+          </a>
+          <span>and</span>
+          <a
+            className={'px-1 text-blue-400 underline'}
+            href={'https://www.shinkai.com/privacy-policy'}
+            rel="noreferrer"
+            target={'_blank'}
+          >
+            Privacy Policy
+          </a>
+        </span>
+      </p>
     </div>
   );
 }
