64a343dbe37302ab47e8723acdb207619e668b74
Paul Ccari <46382556+paulclindo@users.noreply.github.com>
2024-01-12 19:37:48 -0500

    Feat: add advanced connection options  (#116)
    * feat: add advance settings connections
    
    * fix: intl
    
    * fix: set default shinkai identity

--- libs/shinkai-ui/src/index.ts
+++ libs/shinkai-ui/src/index.ts
@@ -2,6 +2,7 @@ export * from './assets/icons';
 
 export * from './components/alert';
 export * from './components/alert-dialog';
+export * from './components/accordion';
 export * from './components/avatar';
 export * from './components/button';
 export * from './components/checkbox';
