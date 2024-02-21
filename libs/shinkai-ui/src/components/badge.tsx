4bb580f791cbd1065688e4abd928db6c2aa20b91
paulclindo <paulclindo@gmail.com>
2024-02-21 15:05:18 -0500

    feat:implement badge + endornments to input component

---
+++ libs/shinkai-ui/src/components/badge.tsx
@@ -0,0 +1,45 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react';
+
+import { cn } from '../utils';
+
+const badgeVariants = cva(
+  'focus:ring-ring inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2',
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-primary text-primary-foreground hover:bg-primary/80 border-transparent shadow',
+        secondary:
+          'bg-secondary text-secondary-foreground hover:bg-secondary/80 border-transparent',
+        destructive:
+          'bg-destructive text-destructive-foreground hover:bg-destructive/80 border-transparent shadow',
+        outline: 'text-foreground',
+        inputAdornment: 'bg-gray-600 px-2 font-medium text-white',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+const Badge = React.forwardRef<HTMLDivElement, BadgeProps>(
+  ({ className, variant, ...props }, ref) => {
+    return (
+      <div
+        className={cn(badgeVariants({ variant }), className)}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+
+Badge.displayName = 'Badge';
+
+export { Badge, badgeVariants };
--- libs/shinkai-ui/src/components/input.tsx
+++ libs/shinkai-ui/src/components/input.tsx
@@ -1,23 +1,66 @@
 import * as React from 'react';
+import { useRef } from 'react';
 
 import { cn } from '../utils';
+import { Badge } from './badge';
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  startAdornment?: React.ReactNode;
+  endAdornment?: React.ReactNode;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, startAdornment, endAdornment, ...props }, ref) => {
+    const startAdornmentRef = useRef<HTMLDivElement>(null);
+    const endAdornmentRef = useRef<HTMLDivElement>(null);
+
+    const style: React.CSSProperties = {};
+    if (startAdornment) {
+      style.paddingLeft = `${
+        (startAdornmentRef?.current?.offsetWidth ?? 0) + 20
+      }px`;
+    }
+    if (endAdornment) {
+      style.paddingRight = `${
+        (endAdornmentRef?.current?.offsetWidth ?? 0) + 20
+      }px`;
+    }
+
     return (
-      <input
-        className={cn(
-          'h-input disabled:text-gray-80 peer w-full rounded-lg border border-gray-200 bg-gray-400 px-4 py-3 pt-8 text-sm font-medium text-white placeholder-transparent outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-gray-200 focus:border focus:border-gray-100 focus:outline-0 disabled:border-0 disabled:bg-gray-400',
-          className,
-        )}
-        placeholder=" "
-        ref={ref}
-        type={type}
-        {...props}
-      />
+      <>
+        <input
+          className={cn(
+            'h-input disabled:text-gray-80 peer w-full rounded-lg border border-gray-200 bg-gray-400 px-4 py-3 pt-8 text-sm font-medium text-white placeholder-transparent outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-gray-200 focus:border focus:border-gray-100 focus:outline-0 disabled:border-0 disabled:bg-gray-400',
+            startAdornment && 'pl-[var(--custom-padding-left-input)]',
+            endAdornment && 'pr-[var(--custom-padding-right-input)]',
+            className,
+          )}
+          placeholder=" "
+          ref={ref}
+          style={style}
+          type={type}
+          {...props}
+        />
+        {startAdornment ? (
+          <Badge
+            className="absolute left-4 top-[30px]"
+            ref={startAdornmentRef}
+            variant="inputAdornment"
+          >
+            {startAdornment}
+          </Badge>
+        ) : null}
+        {endAdornment ? (
+          <Badge
+            className="absolute right-4 top-[30px]"
+            ref={endAdornmentRef}
+            variant="inputAdornment"
+          >
+            {endAdornment}
+          </Badge>
+        ) : null}
+      </>
     );
   },
 );
