7a2f1fb16735fb49492092e849b59329a5fcbe93
paulclindo <paulclindo@gmail.com>
2024-03-01 22:37:12 -0500

    add details page

--- apps/shinkai-visor/src/components/node-files/node-files.tsx
+++ apps/shinkai-visor/src/components/node-files/node-files.tsx
@@ -15,7 +15,7 @@ import {
   FileTypeIcon,
   ScrollArea,
 } from '@shinkai_network/shinkai-ui';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { ChevronLeft, ChevronRight, LockIcon } from 'lucide-react';
 import React from 'react';
 
 import { Header } from '../header/header';
@@ -123,17 +123,76 @@ export default function NodeFiles() {
         }}
         open={!!activeFile}
       >
-        <DrawerContent>
+        <DrawerContent className="my-6">
           <DrawerHeader>
-            <DrawerTitle>Information</DrawerTitle>
+            <DrawerTitle className={'sr-only'}>Information</DrawerTitle>
             <DrawerDescription>
-              <p>Permissions</p>
-              <p className="text-gray-80">You can read and write</p>
+              <div className="space-y-2 text-left">
+                <div>
+                  <FileTypeIcon className="h-10 w-10" />
+                </div>
+                <p className="text-lg font-medium text-white">
+                  {activeFile?.name}
+                  <Badge className="text-gray-80 ml-2 bg-gray-400 text-xs uppercase">
+                    {activeFile?.file_extension?.split('.').pop()}
+                  </Badge>
+                </p>
+                <p className="text-sm text-gray-100">
+                  <span>
+                    {new Date(
+                      activeFile?.creation_date ?? '',
+                    ).toLocaleDateString('en-US', {
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric',
+                    })}
+                  </span>{' '}
+                  - <span>{activeFile?.size}</span>
+                </p>
+              </div>
+              <div className="py-6">
+                <h2 className="mb-3 text-left text-lg font-medium  text-white">
+                  Information
+                </h2>
+                <div className="divide-y divide-gray-300">
+                  {[
+                    { label: 'Created', value: '2021-10-10' },
+                    { label: 'Modified', value: '2021-10-10' },
+                    { label: 'Last Opened', value: '2021-10-10' },
+                  ].map((item) => (
+                    <div
+                      className="flex items-center justify-between py-2 font-medium"
+                      key={item.label}
+                    >
+                      <span className="text-gray-100">Created</span>
+                      <span className="text-white">
+                        {new Date(item.value ?? '').toLocaleDateString(
+                          'en-US',
+                          {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric',
+                          },
+                        )}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="py-6 text-left">
+                <h2 className="mb-3 text-lg font-medium  text-white">
+                  Permissions
+                </h2>
+                <span>
+                  <LockIcon className="mr-2 inline-block h-4 w-4" />
+                  You can read and write
+                </span>
+              </div>
             </DrawerDescription>
           </DrawerHeader>
           <DrawerFooter>
             <Button>Download Source File</Button>
-            <Button variant="ghost">Download Vector Resource</Button>
+            <Button variant="outline">Download Vector Resource</Button>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
