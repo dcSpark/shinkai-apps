ed770c15810957a4f75d2a02716a29a4f9a6405e
Alfredo Gallardo <agallardol@outlook.com>
2024-03-07 10:54:36 -0300

    - feature: added shinkai-node execution

--- apps/shinkai-tray/src-tauri/src/main.rs
+++ apps/shinkai-tray/src-tauri/src/main.rs
@@ -1,14 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::collections::HashMap;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::BackendSpecificError;
 use db::db::TrayDB;
 use models::setup_data::SetupData;
+use shinkai_node::shinkai_node_manager;
 use std::sync::{Arc, Mutex};
 use std::thread;
-use tauri::{AppHandle, CustomMenuItem, GlobalShortcutManager, SystemTray, SystemTrayEvent, SystemTrayMenu, Wry};
+use tauri::{AppHandle, CustomMenuItem, GlobalShortcutManager, RunEvent, SystemTray, SystemTrayEvent, SystemTrayMenu, Wry};
 use tauri::{Manager, SystemTrayMenuItem};
+use tauri::api::process::{Command, CommandEvent};
 use whisper_rs::{FullParams, SamplingStrategy, WhisperContext};
 
 mod audio;
@@ -16,16 +19,17 @@ mod db;
 mod models;
 mod shinkai;
 
+mod shinkai_node;
+
 use audio::transcribe::run;
 use shinkai::registration::process_onboarding_data;
 
 use crate::shinkai::registration::validate_setup_data;
+use crate::shinkai_node::shinkai_node_manager::ShinkaiNodeManager;
 
 enum EmitableEvents {
     CreateJobSection,
 }
-
-
 
 fn main() {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
@@ -33,13 +37,15 @@ fn main() {
     let activate_deactivate = CustomMenuItem::new("activate_deactivate".to_string(), "Activate");
     let create_task = CustomMenuItem::new("create_task".to_string(), "Create Task");
     let settings = CustomMenuItem::new("settings".to_string(), "Settings");
-
+    let toggle_shinkai_node = CustomMenuItem::new("toggle_shinkai_node".to_string(), "Start Shinkai Node");
     let tray_menu = SystemTrayMenu::new()
         .add_item(hide_show)
         .add_native_item(SystemTrayMenuItem::Separator)
         .add_item(activate_deactivate.clone())
         .add_item(create_task)
         .add_item(settings)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(toggle_shinkai_node)
         .add_native_item(SystemTrayMenuItem::Separator)
         .add_item(quit);
 
@@ -48,40 +54,12 @@ fn main() {
     let is_activated = Arc::new(Mutex::new(false)); // change to true
     let is_activated_clone = Arc::clone(&is_activated);
 
-    // // Create a new WhisperContext
-    // let ctx = Arc::new(Mutex::new(
-    //     WhisperContext::new("./models/ggml-base-q5_1.bin").expect("failed to load model"),
-    // ));
-    // let ctx_clone = Arc::clone(&ctx);
-
-    // // Start a new thread for audio capture
-    // thread::spawn(move || {
-    //     let host = cpal::default_host();
-    //     let device = host
-    //         .input_devices()
-    //         .unwrap()
-    //         .find(|d| d.name().unwrap() == "MacBook Pro Microphone")
-    //         .expect("Failed to get MacBook Pro Microphone");
-
-    //     println!("Selected input device: {}", device.name().unwrap());
-    //     println!("Default input config: {:?}", device.default_input_config().unwrap());
-
-    //     let config = device
-    //         .default_input_config()
-    //         .expect("Failed to get default input config");
-
-    //     let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
-
-    //     match config.sample_format() {
-    //         cpal::SampleFormat::F32 => run::<f32>(&device, config.into(), err_fn, is_activated_clone, ctx_clone),
-    //         cpal::SampleFormat::I16 => run::<i16>(&device, config.into(), err_fn, is_activated_clone, ctx_clone),
-    //         cpal::SampleFormat::U16 => run::<u16>(&device, config.into(), err_fn, is_activated_clone, ctx_clone),
-    //         _ => panic!("unsupported sample format"),
-    //     }
-    // });
-
     let db = TrayDB::new("db/tauri").unwrap();
 
+    // Example usage
+    let shinkai_node_manager = Arc::new(Mutex::new(ShinkaiNodeManager::new()));
+
+    let shinkai_node_manager_clone = Arc::clone(&shinkai_node_manager);
 
     tauri::Builder::default()
         .manage(db)
@@ -162,6 +140,16 @@ fn main() {
                     let window = app.get_window("main").unwrap();
                     window.emit("settings", ()).unwrap();
                 }
+                "toggle_shinkai_node" => {
+                    let shinkai_node_manager = shinkai_node_manager.lock().unwrap();
+                    if shinkai_node_manager.is_running() {
+                        shinkai_node_manager.kill_shinkai_node();
+                        let _ = app.tray_handle().get_item("toggle_shinkai_node").set_title("Start Shinkai Node");
+                    } else {
+                        shinkai_node_manager.spawn_shinkai_node();
+                        let _ = app.tray_handle().get_item("toggle_shinkai_node").set_title("Stop Shinkai Node");
+                    }
+                }
                 "quit" => {
                     std::process::exit(0);
                 }
@@ -169,21 +157,16 @@ fn main() {
             },
             _ => {}
         })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(move |app_handle, event| match event {
+            RunEvent::ExitRequested { api, .. } => {
+                let shinkai_node_manager = shinkai_node_manager_clone.lock().unwrap();
+                if shinkai_node_manager.is_running() {
+                    shinkai_node_manager.kill_shinkai_node();
+                }
+            }
+            _ => {}
+        });
 }
 
-// Previous
-
-// // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-// #[tauri::command]
-// fn greet(name: &str) -> String {
-//     format!("Hello, {}! You've been greeted from Rust!", name)
-// }
-
-// fn main() {
-//     tauri::Builder::default()
-//         .invoke_handler(tauri::generate_handler![greet])
-//         .run(tauri::generate_context!())
-//         .expect("error while running tauri application");
-// }
