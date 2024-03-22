d16a935ebc1d930c185ee0eae55a1c70fbadc9b6
paulclindo <paulclindo@gmail.com>
2024-03-22 00:32:30 -0600

    feat: autofocus on drawer

--- apps/shinkai-visor/src/components/node-files/node-files.tsx
+++ apps/shinkai-visor/src/components/node-files/node-files.tsx
@@ -1,7 +1,4 @@
-import { zodResolver } from '@hookform/resolvers/zod';
 import { HomeIcon } from '@radix-ui/react-icons';
-import { useCreateVRFolder } from '@shinkai_network/shinkai-node-state/lib/mutations/createVRFolder/useCreateVRFolder';
-import { useUploadVRFiles } from '@shinkai_network/shinkai-node-state/lib/mutations/uploadVRFiles/useUploadVRFiles';
 import {
   VRFolder,
   VRItem,
@@ -9,7 +6,6 @@ import {
 import { useGetVRPathSimplified } from '@shinkai_network/shinkai-node-state/lib/queries/getVRPathSimplified/useGetVRPathSimplified';
 import {
   AddNewFolderIcon,
-  Badge,
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
@@ -22,32 +18,18 @@ import {
   ContextMenuItem,
   ContextMenuTrigger,
   CreateAIIcon,
-  DirectoryTypeIcon,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
   FileEmptyStateIcon,
-  FileTypeIcon,
-  FileUploader,
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
   GenerateDocIcon,
   Input,
   ScrollArea,
-  TextField,
   ToggleGroup,
   ToggleGroupItem,
 } from '@shinkai_network/shinkai-ui';
 import { cn } from '@shinkai_network/shinkai-ui/utils';
-import { partial } from 'filesize';
 import { motion } from 'framer-motion';
 import {
   ChevronRight,
@@ -57,20 +39,14 @@ import {
   // FileIcon,
   // DatabaseIcon,
   // FileIcon,
-  LockIcon,
   PlusIcon,
   SearchIcon,
   X,
   XIcon,
 } from 'lucide-react';
-import React, { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import { FormattedMessage } from 'react-intl';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { toast } from 'sonner';
-import { z } from 'zod';
 
-import { formatDateToLocaleString } from '../../helpers/date';
 import { useAuth } from '../../store/auth/auth';
 import { Header } from '../header/header';
 import { useVectorFsStore, VectorFSLayout } from './node-file-context';
@@ -460,266 +436,3 @@ export default function NodeFiles() {
     </div>
   );
 }
-
-export const VectorFileDetails = () => {
-  const selectedFile = useVectorFsStore((state) => state.selectedFile);
-  const size = partial({ standard: 'jedec' });
-
-  return (
-    <React.Fragment>
-      <DrawerHeader>
-        <DrawerTitle className={'sr-only'}>Information</DrawerTitle>
-      </DrawerHeader>
-      <div>
-        <div className="space-y-2 text-left">
-          <div>
-            <FileTypeIcon className="h-10 w-10" />
-          </div>
-          <p className="text-lg font-medium text-white">
-            {selectedFile?.name}
-            <Badge className="text-gray-80 ml-2 bg-gray-400 text-xs uppercase">
-              {selectedFile?.vr_header?.resource_source?.Reference?.FileRef
-                ?.file_type?.Document ?? '-'}
-            </Badge>
-          </p>
-          <p className="text-sm text-gray-100">
-            <span>
-              {formatDateToLocaleString(selectedFile?.created_datetime)}
-            </span>{' '}
-            - <span>{size(selectedFile?.vr_size ?? 0)}</span>
-          </p>
-        </div>
-        <div className="py-6">
-          <h2 className="mb-3 text-left text-lg font-medium  text-white">
-            Information
-          </h2>
-          <div className="divide-y divide-gray-300">
-            {[
-              { label: 'Created', value: selectedFile?.created_datetime },
-              {
-                label: 'Modified',
-                value: selectedFile?.last_written_datetime,
-              },
-              {
-                label: 'Last Opened',
-                value: selectedFile?.last_read_datetime,
-              },
-            ].map((item) => (
-              <div
-                className="flex items-center justify-between py-2 font-medium"
-                key={item.label}
-              >
-                <span className="text-gray-100">{item.label}</span>
-                <span className="text-white">
-                  {formatDateToLocaleString(item.value)}
-                </span>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="py-6 text-left">
-          <h2 className="mb-3 text-lg font-medium  text-white">Permissions</h2>
-          <span>
-            <LockIcon className="mr-2 inline-block h-4 w-4" />
-            You can read and write
-          </span>
-        </div>
-      </div>
-
-      <DrawerFooter>
-        <Button>Download Source File</Button>
-        <Button variant="outline">Download Vector Resource</Button>
-      </DrawerFooter>
-    </React.Fragment>
-  );
-};
-
-const createFolderSchema = z.object({
-  name: z.string(),
-});
-export const AddNewFolderDrawer = () => {
-  const auth = useAuth((state) => state.auth);
-  const currentGlobalPath = useVectorFsStore(
-    (state) => state.currentGlobalPath,
-  );
-  const closeDrawerMenu = useVectorFsStore((state) => state.closeDrawerMenu);
-  const createFolderForm = useForm<z.infer<typeof createFolderSchema>>({
-    resolver: zodResolver(createFolderSchema),
-  });
-
-  const {
-    isPending,
-    mutateAsync: createVRFolder,
-    isSuccess,
-  } = useCreateVRFolder({
-    onSuccess: () => {
-      toast.success('Folder created successfully');
-      createFolderForm.reset();
-      closeDrawerMenu();
-    },
-    onError: () => {
-      toast.error('Error creating folder');
-    },
-  });
-
-  const onSubmit = async (values: z.infer<typeof createFolderSchema>) => {
-    if (!auth) return;
-
-    await createVRFolder({
-      nodeAddress: auth?.node_address ?? '',
-      profile: auth?.profile ?? '',
-      shinkaiIdentity: auth?.shinkai_identity ?? '',
-      folderName: values.name,
-      path: currentGlobalPath,
-      my_device_encryption_sk: auth?.profile_encryption_sk ?? '',
-      my_device_identity_sk: auth?.profile_identity_sk ?? '',
-      node_encryption_pk: auth?.node_encryption_pk ?? '',
-      profile_encryption_sk: auth?.profile_encryption_sk ?? '',
-      profile_identity_sk: auth?.profile_identity_sk ?? '',
-    });
-  };
-
-  useEffect(() => {
-    if (isSuccess) {
-      toast.success('Folder created successfully');
-      createFolderForm.reset();
-    }
-  }, [createFolderForm, isSuccess]);
-
-  return (
-    <>
-      <DrawerHeader>
-        <DrawerTitle className="flex flex-col items-start gap-1">
-          <DirectoryTypeIcon className="h-10 w-10" />
-          Add New Folder
-        </DrawerTitle>
-      </DrawerHeader>
-      <Form {...createFolderForm}>
-        <form
-          className="space-y-8 pt-4"
-          onSubmit={createFolderForm.handleSubmit(onSubmit)}
-        >
-          <FormField
-            control={createFolderForm.control}
-            name="name"
-            render={({ field }) => (
-              <TextField field={field} label="Folder Name" />
-            )}
-          />
-          <Button
-            className="w-full"
-            disabled={isPending}
-            isLoading={isPending}
-            type="submit"
-          >
-            Create Folder
-          </Button>
-        </form>
-      </Form>
-    </>
-  );
-};
-const uploadVRFilesSchema = z.object({
-  files: z.array(z.any()).max(3),
-});
-export const UploadVRFilesDrawer = () => {
-  const auth = useAuth((state) => state.auth);
-  const closeDrawerMenu = useVectorFsStore((state) => state.closeDrawerMenu);
-  const currentGlobalPath = useVectorFsStore(
-    (state) => state.currentGlobalPath,
-  );
-  const createFolderForm = useForm<z.infer<typeof uploadVRFilesSchema>>({
-    resolver: zodResolver(uploadVRFilesSchema),
-  });
-
-  const { isPending, mutateAsync: uploadVRFiles } = useUploadVRFiles({
-    onSuccess: () => {
-      toast.success('Files uploaded successfully', {
-        id: 'uploading-VR-files',
-        description: '',
-      });
-      createFolderForm.reset();
-    },
-    onError: () => {
-      toast.error('Error uploading files', {
-        id: 'uploading-VR-files',
-        description: '',
-      });
-    },
-  });
-
-  const onSubmit = async (values: z.infer<typeof uploadVRFilesSchema>) => {
-    if (!auth) return;
-    toast.loading('Uploading files', {
-      id: 'uploading-VR-files',
-      description: 'This process might take from 1-2 minutes',
-      position: 'bottom-left',
-    });
-    closeDrawerMenu();
-    await uploadVRFiles({
-      nodeAddress: auth?.node_address ?? '',
-      sender: auth?.shinkai_identity ?? '',
-      senderSubidentity: auth?.profile ?? '',
-      receiver: auth?.shinkai_identity ?? '',
-      destinationPath: currentGlobalPath,
-      files: values.files,
-      my_device_encryption_sk: auth?.profile_encryption_sk ?? '',
-      my_device_identity_sk: auth?.profile_identity_sk ?? '',
-      node_encryption_pk: auth?.node_encryption_pk ?? '',
-      profile_encryption_sk: auth?.profile_encryption_sk ?? '',
-      profile_identity_sk: auth?.profile_identity_sk ?? '',
-    });
-  };
-
-  return (
-    <>
-      <DrawerHeader>
-        <DrawerTitle className="flex flex-col items-start gap-1">
-          <FileTypeIcon className="h-10 w-10" />
-          File Upload
-        </DrawerTitle>
-      </DrawerHeader>
-      <Form {...createFolderForm}>
-        <form
-          className="space-y-8"
-          onSubmit={createFolderForm.handleSubmit(onSubmit)}
-        >
-          <FormField
-            control={createFolderForm.control}
-            name="files"
-            render={({ field }) => (
-              <FormItem className="mt-3">
-                <FormLabel className="sr-only">
-                  <FormattedMessage id="file.one" />
-                </FormLabel>
-                <FormControl>
-                  <div className="flex flex-col space-y-1">
-                    <div className="flex items-center justify-center">
-                      <FileUploader
-                        allowMultiple
-                        descriptionText="Supports pdf, md, txt"
-                        onChange={(acceptedFiles) => {
-                          field.onChange(acceptedFiles);
-                        }}
-                        value={field.value}
-                      />
-                    </div>
-                  </div>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button
-            className="w-full"
-            disabled={isPending}
-            isLoading={isPending}
-            type="submit"
-          >
-            Upload
-          </Button>
-        </form>
-      </Form>
-    </>
-  );
-};
---
+++ apps/shinkai-visor/src/components/node-files/vector-fs-item-detail.tsx
@@ -0,0 +1,87 @@
+import {
+  Badge,
+  Button,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  FileTypeIcon,
+} from '@shinkai_network/shinkai-ui';
+import { partial } from 'filesize';
+import { LockIcon } from 'lucide-react';
+import React from 'react';
+
+import { formatDateToLocaleString } from '../../helpers/date';
+import { useVectorFsStore } from './node-file-context';
+
+export const VectorFileDetails = () => {
+  const selectedFile = useVectorFsStore((state) => state.selectedFile);
+  const size = partial({ standard: 'jedec' });
+
+  return (
+    <React.Fragment>
+      <DrawerHeader>
+        <DrawerTitle className={'sr-only'}>Information</DrawerTitle>
+      </DrawerHeader>
+      <div>
+        <div className="space-y-2 text-left">
+          <div>
+            <FileTypeIcon className="h-10 w-10" />
+          </div>
+          <p className="text-lg font-medium text-white">
+            {selectedFile?.name}
+            <Badge className="text-gray-80 ml-2 bg-gray-400 text-xs uppercase">
+              {selectedFile?.vr_header?.resource_source?.Reference?.FileRef
+                ?.file_type?.Document ?? '-'}
+            </Badge>
+          </p>
+          <p className="text-sm text-gray-100">
+            <span>
+              {formatDateToLocaleString(selectedFile?.created_datetime)}
+            </span>{' '}
+            - <span>{size(selectedFile?.vr_size ?? 0)}</span>
+          </p>
+        </div>
+        <div className="py-6">
+          <h2 className="mb-3 text-left text-lg font-medium  text-white">
+            Information
+          </h2>
+          <div className="divide-y divide-gray-300">
+            {[
+              { label: 'Created', value: selectedFile?.created_datetime },
+              {
+                label: 'Modified',
+                value: selectedFile?.last_written_datetime,
+              },
+              {
+                label: 'Last Opened',
+                value: selectedFile?.last_read_datetime,
+              },
+            ].map((item) => (
+              <div
+                className="flex items-center justify-between py-2 font-medium"
+                key={item.label}
+              >
+                <span className="text-gray-100">{item.label}</span>
+                <span className="text-white">
+                  {formatDateToLocaleString(item.value)}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="py-6 text-left">
+          <h2 className="mb-3 text-lg font-medium  text-white">Permissions</h2>
+          <span>
+            <LockIcon className="mr-2 inline-block h-4 w-4" />
+            You can read and write
+          </span>
+        </div>
+      </div>
+
+      <DrawerFooter>
+        <Button>Download Source File</Button>
+        <Button variant="outline">Download Vector Resource</Button>
+      </DrawerFooter>
+    </React.Fragment>
+  );
+};
