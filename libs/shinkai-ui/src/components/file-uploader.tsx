edb1ee8047004952d6ac8730839254a409d6740b
paulclindo <paulclindo@gmail.com>
2024-04-03 18:37:08 -0500

    fix scroll files area

--- libs/shinkai-ui/src/components/file-uploader.tsx
+++ libs/shinkai-ui/src/components/file-uploader.tsx
@@ -2,7 +2,7 @@ import { Trash, Upload } from 'lucide-react';
 import React from 'react';
 import { Accept, useDropzone } from 'react-dropzone';
 
-import { Button, PaperClipIcon } from '../index';
+import { Button, PaperClipIcon, ScrollArea } from '../index';
 
 const FileUploader = ({
   value,
@@ -52,32 +52,35 @@ const FileUploader = ({
 
         <input {...getInputFileProps({})} />
       </div>
+
       {!!value?.length && (
-        <div className="flex flex-col gap-2">
-          {value?.map((file, idx) => (
-            <div
-              className="relative flex items-center gap-2 rounded-lg border border-gray-100 px-3 py-1.5"
-              key={idx}
-            >
-              <PaperClipIcon className="text-gray-100" />
-              <span className="text-gray-80 flex-1 truncate text-sm">
-                {file.name}
-              </span>
-              <Button
-                onClick={() => {
-                  const newFiles = [...value];
-                  newFiles.splice(newFiles.indexOf(file), 1);
-                  onChange(newFiles);
-                }}
-                size="icon"
-                type="button"
-                variant="ghost"
+        <ScrollArea className="max-h-[40vh] flex-1 grow overflow-y-scroll  pr-2 [&>div>div]:!block">
+          <div className="flex flex-col gap-2">
+            {value?.map((file, idx) => (
+              <div
+                className="relative flex items-center gap-2 rounded-lg border border-gray-100 px-3 py-1.5"
+                key={idx}
               >
-                <Trash className="h-4 w-4 text-gray-100" />
-              </Button>
-            </div>
-          ))}
-        </div>
+                <PaperClipIcon className="text-gray-100" />
+                <span className="text-gray-80 flex-1 truncate text-sm">
+                  {file.name}
+                </span>
+                <Button
+                  onClick={() => {
+                    const newFiles = [...value];
+                    newFiles.splice(newFiles.indexOf(file), 1);
+                    onChange(newFiles);
+                  }}
+                  size="icon"
+                  type="button"
+                  variant="ghost"
+                >
+                  <Trash className="h-4 w-4 text-gray-100" />
+                </Button>
+              </div>
+            ))}
+          </div>
+        </ScrollArea>
       )}
     </div>
   );
