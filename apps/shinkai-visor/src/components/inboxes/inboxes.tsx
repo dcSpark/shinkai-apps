a5f06992e46f3f1bf6c2893ff6f962d8f21c904e
paulclindo <paulclindo@gmail.com>
2024-04-24 18:57:19 -0500

    fix: inboxes skeleton loader

--- apps/shinkai-visor/src/components/inboxes/inboxes.tsx
+++ apps/shinkai-visor/src/components/inboxes/inboxes.tsx
@@ -41,7 +41,11 @@ export const Inboxes = () => {
   //     inboxId: '',
   //     name: '',
   //   });
-  const { inboxes } = useGetInboxes({
+  const {
+    inboxes,
+    isSuccess: isInboxesSuccess,
+    isPending: isInboxesPending,
+  } = useGetInboxes({
     nodeAddress: auth?.node_address ?? '',
     sender: auth?.shinkai_identity ?? '',
     senderSubidentity: auth?.profile ?? '',
@@ -54,7 +58,7 @@ export const Inboxes = () => {
     profile_encryption_sk: auth?.profile_encryption_sk ?? '',
     profile_identity_sk: auth?.profile_identity_sk ?? '',
   });
-  const { agents } = useAgents({
+  const { agents, isSuccess: isAgentsSuccess } = useAgents({
     nodeAddress: auth?.node_address ?? '',
     sender: auth?.shinkai_identity ?? '',
     senderSubidentity: `${auth?.profile}`,
@@ -106,109 +110,124 @@ export const Inboxes = () => {
 
   return (
     <div className="flex h-full flex-col justify-between space-y-3 overflow-hidden">
-      {!agents?.length ? (
-        <EmptyAgents data-testid="empty-agents" />
-      ) : !inboxes?.length ? (
-        <EmptyInboxes data-testid="empty-inboxes" />
-      ) : (
-        <>
-          <div className="flex grow flex-col overflow-hidden">
-            <ScrollArea className="pr-4 [&>div>div]:!block">
-              <Tabs defaultValue="actives">
-                <TabsList className="grid w-full grid-cols-2">
-                  <TabsTrigger
-                    className="flex items-center gap-1.5"
-                    value="actives"
-                  >
-                    <ActiveIcon className="h-4 w-4" />
-                    Active
-                  </TabsTrigger>
-                  <TabsTrigger
-                    className="flex items-center gap-1.5"
-                    value="archives"
-                  >
-                    <ArchiveIcon className="h-4 w-4" />
-                    Archived
-                  </TabsTrigger>
-                </TabsList>
-                <TabsContent value="actives">
-                  <div className="space-y-4" data-testid="inboxes-container">
-                    {activesInboxes?.length ? (
-                      activesInboxes.map((inbox) => (
-                        <ActiveInboxItem inbox={inbox} key={inbox.inbox_id} />
-                      ))
-                    ) : (
-                      <p className="py-5 text-center">
-                        No actives conversations found.{' '}
-                      </p>
-                    )}
-                  </div>
-                </TabsContent>
-                <TabsContent value="archives">
-                  <div className="space-y-4" data-testid="inboxes-container">
-                    {archivesInboxes?.length ? (
-                      archivesInboxes.map((inbox) => (
-                        <ArchiveInboxItem inbox={inbox} key={inbox.inbox_id} />
-                      ))
-                    ) : (
-                      <p className="py-5 text-center">
-                        No archived conversations found.{' '}
-                      </p>
-                    )}
-                  </div>
-                </TabsContent>
-              </Tabs>
-            </ScrollArea>
-          </div>
-          <div className="fixed bottom-4 right-4" ref={dialContainerRef}>
-            {auth?.shinkai_identity.includes('localhost') ? (
-              <Button
-                className="h-[60px] w-[60px]"
-                onClick={() => onCreateJobClick()}
-                size="icon"
-              >
-                <Plus />
-              </Button>
-            ) : (
-              <DropdownMenu onOpenChange={(isOpen) => setDialOpened(isOpen)}>
-                <DropdownMenuTrigger asChild>
-                  <Button className="h-[60px] w-[60px]" size="icon">
-                    <Plus
-                      className={cn(
-                        'h-7 w-7 transition-transform',
-                        dialOpened && 'rotate-45',
-                      )}
-                    />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuPortal container={dialContainerRef.current}>
-                  <DropdownMenuContent align="end" className="px-2.5 py-2">
-                    <DropdownMenuItem onClick={() => onCreateJobClick()}>
-                      <JobBubbleIcon className="mr-2 h-4 w-4" />
-                      <span>
-                        <FormattedMessage id="create-job" />
-                      </span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => onCreateInboxClick()}>
-                      <ChatBubbleIcon className="mr-2 h-4 w-4" />
-                      <span>
-                        <FormattedMessage id="create-inbox" />
-                      </span>
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenuPortal>
-              </DropdownMenu>
-            )}
-          </div>
-          {/*<EditInboxNameDialog*/}
-          {/*  inboxId={isEditInboxNameDialogOpened.inboxId || ''}*/}
-          {/*  name={isEditInboxNameDialogOpened.name}*/}
-          {/*  onCancel={() => closeEditInboxNameDialog()}*/}
-          {/*  onSaved={() => closeEditInboxNameDialog()}*/}
-          {/*  open={isEditInboxNameDialogOpened.isOpened}*/}
-          {/*/>*/}
-        </>
+      {isAgentsSuccess &&
+        isInboxesSuccess &&
+        !agents?.length &&
+        !inboxes.length && <EmptyAgents data-testid="empty-agents" />}
+
+      {isInboxesSuccess &&
+        isAgentsSuccess &&
+        !inboxes?.length &&
+        agents?.length > 0 && <EmptyInboxes data-testid="empty-inboxes" />}
+
+      {isInboxesPending && (
+        <div className="flex flex-col gap-2">
+          {Array.from({ length: 4 }).map((_, idx) => (
+            <div
+              className="mb-1 flex h-[69px] items-center justify-between gap-2 rounded-lg bg-gray-400 py-3"
+              key={idx}
+            />
+          ))}
+        </div>
       )}
+
+      {isInboxesSuccess && inboxes.length > 0 && (
+        <div className="flex grow flex-col overflow-hidden">
+          <ScrollArea className="pr-4 [&>div>div]:!block">
+            <Tabs defaultValue="actives">
+              <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger
+                  className="flex items-center gap-1.5"
+                  value="actives"
+                >
+                  <ActiveIcon className="h-4 w-4" />
+                  Active
+                </TabsTrigger>
+                <TabsTrigger
+                  className="flex items-center gap-1.5"
+                  value="archives"
+                >
+                  <ArchiveIcon className="h-4 w-4" />
+                  Archived
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="actives">
+                <div className="space-y-4" data-testid="inboxes-container">
+                  {activesInboxes?.length ? (
+                    activesInboxes.map((inbox) => (
+                      <ActiveInboxItem inbox={inbox} key={inbox.inbox_id} />
+                    ))
+                  ) : (
+                    <p className="py-5 text-center">
+                      No actives conversations found.{' '}
+                    </p>
+                  )}
+                </div>
+              </TabsContent>
+              <TabsContent value="archives">
+                <div className="space-y-4" data-testid="inboxes-container">
+                  {archivesInboxes?.length ? (
+                    archivesInboxes.map((inbox) => (
+                      <ArchiveInboxItem inbox={inbox} key={inbox.inbox_id} />
+                    ))
+                  ) : (
+                    <p className="py-5 text-center">
+                      No archived conversations found.{' '}
+                    </p>
+                  )}
+                </div>
+              </TabsContent>
+            </Tabs>
+          </ScrollArea>
+        </div>
+      )}
+      <div className="fixed bottom-4 right-4" ref={dialContainerRef}>
+        {auth?.shinkai_identity.includes('localhost') ? (
+          <Button
+            className="h-[60px] w-[60px]"
+            onClick={() => onCreateJobClick()}
+            size="icon"
+          >
+            <Plus />
+          </Button>
+        ) : (
+          <DropdownMenu onOpenChange={(isOpen) => setDialOpened(isOpen)}>
+            <DropdownMenuTrigger asChild>
+              <Button className="h-[60px] w-[60px]" size="icon">
+                <Plus
+                  className={cn(
+                    'h-7 w-7 transition-transform',
+                    dialOpened && 'rotate-45',
+                  )}
+                />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuPortal container={dialContainerRef.current}>
+              <DropdownMenuContent align="end" className="px-2.5 py-2">
+                <DropdownMenuItem onClick={() => onCreateJobClick()}>
+                  <JobBubbleIcon className="mr-2 h-4 w-4" />
+                  <span>
+                    <FormattedMessage id="create-job" />
+                  </span>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => onCreateInboxClick()}>
+                  <ChatBubbleIcon className="mr-2 h-4 w-4" />
+                  <span>
+                    <FormattedMessage id="create-inbox" />
+                  </span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenuPortal>
+          </DropdownMenu>
+        )}
+      </div>
+      {/*<EditInboxNameDialog*/}
+      {/*  inboxId={isEditInboxNameDialogOpened.inboxId || ''}*/}
+      {/*  name={isEditInboxNameDialogOpened.name}*/}
+      {/*  onCancel={() => closeEditInboxNameDialog()}*/}
+      {/*  onSaved={() => closeEditInboxNameDialog()}*/}
+      {/*  open={isEditInboxNameDialogOpened.isOpened}*/}
+      {/*/>*/}
     </div>
   );
 };
