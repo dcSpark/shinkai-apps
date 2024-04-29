510579579d429c0b08e294669ef7e0223081c462
paulclindo <paulclindo@gmail.com>
2024-04-29 15:56:31 -0500

    typography fixes

--- libs/shinkai-ui/src/components/markdown-preview.tsx
+++ libs/shinkai-ui/src/components/markdown-preview.tsx
@@ -38,7 +38,9 @@ export const MarkdownPreview = ({
       className={cn(
         'wmde-markdown-var',
         'max-w-none text-white',
-        'prose prose-gray prose-code:text-white prose-blockquote:text-gray-50 prose-blockquote:bg-gray-200 prose-strong:text-white prose-headings:text-white prose-p:whitespace-pre-wrap',
+        'prose prose-gray',
+        'prose-h1:!text-xl prose-h1:!mb-3 prose-h2:!text-lg prose-h2:!mb-2 prose-h3:!text-base',
+        'prose-code:text-white prose-blockquote:text-gray-50 prose-blockquote:bg-gray-200 prose-strong:text-white prose-headings:text-white prose-p:whitespace-pre-wrap',
         'prose-h1:!border-b-0 prose-h2:!border-b-0 prose-h3:!border-b-0 prose-h4:!border-b-0 prose-h5:!border-b-0 prose-h6:!border-b-0',
         className,
       )}
