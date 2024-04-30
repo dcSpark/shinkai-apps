fff102d4e8640b942e9dce1e5b1d307fb46c2085
paulclindo <paulclindo@gmail.com>
2024-04-29 23:31:42 -0500

    fix layout

--- apps/shinkai-desktop/src/pages/layout/simple-layout.tsx
+++ apps/shinkai-desktop/src/pages/layout/simple-layout.tsx
@@ -21,7 +21,7 @@ export const SubpageLayout = ({
       <h1 className="mb-8 text-center text-2xl font-semibold tracking-tight">
         {title}
       </h1>
-      <div>{children}</div>
+      <div className="flex-1">{children}</div>
     </div>
   );
 };
@@ -44,7 +44,7 @@ export const SimpleLayout = ({
       {title ? (
         <h1 className="text-2xl font-semibold tracking-tight">{title}</h1>
       ) : null}
-      <div>{children}</div>
+      <div className="flex-1">{children}</div>
     </div>
   );
 };
