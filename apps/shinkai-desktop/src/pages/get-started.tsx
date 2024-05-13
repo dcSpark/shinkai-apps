24496379862bd9cff36c5fa73a6a3847bb3f31ab
paulclindo <paulclindo@gmail.com>
2024-05-13 18:36:27 -0500

    disable new onboarding

--- apps/shinkai-desktop/src/pages/get-started.tsx
+++ apps/shinkai-desktop/src/pages/get-started.tsx
@@ -1,10 +1,87 @@
-import { buttonVariants, Separator } from '@shinkai_network/shinkai-ui';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useSubmitRegistrationNoCode } from '@shinkai_network/shinkai-node-state/lib/mutations/submitRegistation/useSubmitRegistrationNoCode';
+import { useGetEncryptionKeys } from '@shinkai_network/shinkai-node-state/lib/queries/getEncryptionKeys/useGetEncryptionKeys';
+import { Button, buttonVariants, Separator } from '@shinkai_network/shinkai-ui';
 import { cn } from '@shinkai_network/shinkai-ui/utils';
-import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { z } from 'zod';
 
+import { useAuth } from '../store/auth';
+import { useShinkaiNodeEventsToast } from '../windows/shinkai-node-manager/shinkai-node-manager-hooks';
+import { useShinkaiNodeSpawnMutation } from '../windows/shinkai-node-manager/shinkai-node-process-client';
 import OnboardingLayout from './layout/onboarding-layout';
 
+const formSchema = z.object({
+  registration_name: z.string().min(5),
+  node_address: z.string().url({
+    message: 'Node Address must be a valid URL',
+  }),
+  shinkai_identity: z
+    .string()
+    .regex(
+      /^@@[a-zA-Z0-9_]+\.shinkai.*$/,
+      `It should be in the format of @@<name>.shinkai`,
+    )
+    .nullish(),
+});
+
 const GetStartedPage = () => {
+  const navigate = useNavigate();
+
+  const setAuth = useAuth((state) => state.setAuth);
+  useShinkaiNodeEventsToast();
+  const { encryptionKeys } = useGetEncryptionKeys();
+  const setupDataForm = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      registration_name: 'main_device',
+      node_address: 'http://127.0.0.1:9550',
+    },
+  });
+
+  const { mutateAsync: submitRegistration } = useSubmitRegistrationNoCode({
+    onSuccess: (response, setupPayload) => {
+      if (response.success && encryptionKeys) {
+        const updatedSetupData = {
+          ...encryptionKeys,
+          ...setupPayload,
+          permission_type: '',
+          shinkai_identity:
+            setupPayload.shinkai_identity || (response.data?.node_name ?? ''),
+          node_signature_pk: response.data?.identity_public_key ?? '',
+          node_encryption_pk: response.data?.encryption_public_key ?? '',
+        };
+        setAuth(updatedSetupData);
+        navigate('/');
+      } else {
+        throw new Error('Failed to submit registration');
+      }
+    },
+  });
+  const {
+    isPending: shinkaiNodeSpawnIsPending,
+    mutateAsync: shinkaiNodeSpawn,
+  } = useShinkaiNodeSpawnMutation({
+    onSuccess: () => {
+      onSubmit(setupDataForm.getValues());
+    },
+  });
+
+  async function onSubmit(currentValues: z.infer<typeof formSchema>) {
+    if (!encryptionKeys) return;
+    await submitRegistration({
+      profile: 'main',
+      identity_type: 'device',
+      permission_type: 'admin',
+      shinkai_identity: currentValues.shinkai_identity ?? '',
+      registration_code: '',
+      node_encryption_pk: '',
+      node_address: currentValues.node_address,
+      registration_name: currentValues.registration_name,
+      ...encryptionKeys,
+    });
+  }
   return (
     <OnboardingLayout>
       <div className="flex h-full flex-col">
@@ -13,20 +90,28 @@ const GetStartedPage = () => {
           <span aria-hidden> 🔑</span>
         </p>
         <div className="mt-20 flex flex-1 flex-col gap-10">
-          <Link
-            className={cn(
-              buttonVariants({
-                size: 'lg',
-              }),
-              'w-full',
-            )}
-            state={{ connectionType: 'local' }}
-            to={{
-              pathname: '/onboarding',
-            }}
+          {/* Note: Temporary disabled, model manager and http subscriptions are work in progress */}
+          {/*<Link*/}
+          {/*  className={cn(*/}
+          {/*    buttonVariants({*/}
+          {/*      size: 'lg',*/}
+          {/*    }),*/}
+          {/*    'w-full',*/}
+          {/*  )}*/}
+          {/*  state={{ connectionType: 'local' }}*/}
+          {/*  to={{*/}
+          {/*    pathname: '/onboarding',*/}
+          {/*  }}*/}
+          {/*>*/}
+          {/*  Shinkai Private (Local)*/}
+          {/*</Link>*/}
+          <Button
+            isLoading={shinkaiNodeSpawnIsPending}
+            onClick={() => shinkaiNodeSpawn()}
+            size="lg"
           >
             Shinkai Private (Local)
-          </Link>
+          </Button>
           <Separator className="relative" decorative>
             <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-500 p-2 text-gray-100">
               or
