31bdc0e737854b8c8175dc84b7d0f3a5790ca714
paulclindo <paulclindo@gmail.com>
2024-05-17 19:00:27 -0500

    fix header

--- libs/shinkai-message-ts/src/http-client.ts
+++ libs/shinkai-message-ts/src/http-client.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const httpClient = axios.create({
   timeout: 2 * 60 * 1000, // 2 minutes
-  headers: {
-    'Content-Encoding': 'gzip',
-  },
 });
+
+httpClient.defaults.headers.common['Accept-Encoding'] = 'gzip';
