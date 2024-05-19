1d06ffc4403b2782039aade32b4b38067a87b45e
paulclindo <paulclindo@gmail.com>
2024-05-19 18:18:25 -0500

    refactor

--- apps/shinkai-app/capacitor.config.ts
+++ apps/shinkai-app/capacitor.config.ts
@@ -3,12 +3,12 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.shinkai.app',
   appName: 'Shinkai',
-  webDir: '../../dist/apps/shinkai-app',
+  webDir: 'dist',
   server: {
     androidScheme: 'https',
     url: process.env.SERVE ? 'http://10.0.2.2:9000' : undefined,
     cleartext: process.env.SERVE ? true : false,
-  }
+  },
 };
 
 export default config;
