9d9c55749b3c0efee09ff8dd4b73d2e098eef5e5
paulclindo <paulclindo@gmail.com>
2024-05-20 18:59:18 -0500

    fix: query client

--- apps/shinkai-desktop/src/pages/ai-model-installation.tsx
+++ apps/shinkai-desktop/src/pages/ai-model-installation.tsx
@@ -1,33 +1,37 @@
 import { buttonVariants } from '@shinkai_network/shinkai-ui';
 import { cn } from '@shinkai_network/shinkai-ui/utils';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ArrowRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 import { OllamaModels } from '../components/shinkai-node-manager/ollama-models';
+import { queryClient } from '../lib/shinkai-node-manager/shinkai-node-manager-client';
 import { SubpageLayout } from './layout/simple-layout';
 
 const AIModelInstallation = () => {
   return (
-    <SubpageLayout
-      className="relative flex w-full max-w-6xl flex-col gap-2 px-4"
-      title="Install AI"
-    >
-      <OllamaModels />
-      <Link
-        className={cn(
-          buttonVariants({
-            size: 'auto',
-          }),
-          'absolute right-0 top-9 mx-auto gap-2 px-6 py-2.5',
-        )}
-        to={{
-          pathname: '/',
-        }}
+    <QueryClientProvider client={queryClient}>
+      <SubpageLayout
+        className="relative flex w-full max-w-6xl flex-col gap-2 px-4"
+        title="Install AI"
       >
-        Continue
-        <ArrowRight className="h-4 w-4" />
-      </Link>
-    </SubpageLayout>
+        <OllamaModels />
+        <Link
+          className={cn(
+            buttonVariants({
+              size: 'auto',
+            }),
+            'absolute right-0 top-9 mx-auto gap-2 px-6 py-2.5',
+          )}
+          to={{
+            pathname: '/',
+          }}
+        >
+          Continue
+          <ArrowRight className="h-4 w-4" />
+        </Link>
+      </SubpageLayout>
+    </QueryClientProvider>
   );
 };
 
