73c079bc53dc9cbad7702d0776c2383b59192acf
Alfredo Gallardo <agallardol@outlook.com>
2024-06-03 11:45:53 -0400

    - fix: improve vram detection

--- apps/shinkai-desktop/src-tauri/src/hardware.rs
+++ apps/shinkai-desktop/src-tauri/src/hardware.rs
@@ -1,3 +1,5 @@
+use std::ptr::null;
+
 use serde::Serialize;
 use sysinfo::System;
 use wgpu::DeviceType;
@@ -69,25 +71,40 @@ pub const REQUIREMENTS: Requirements = Requirements {
     },
 };
 
+fn get_total_vram(adapter: &wgpu::Adapter) -> u64 {
+    if adapter.get_info().device_type != DeviceType::DiscreteGpu {
+        let mut sys = System::new_all();
+        sys.refresh_all();
+        return sys.total_memory() / 1024 / 1024 / 1024;
+    }
+
+    let features = adapter.features();
+    if features.contains(wgpu::Features::TEXTURE_ADAPTER_SPECIFIC_FORMAT_FEATURES) {
+        let limits = adapter.limits();
+        limits.max_texture_dimension_1d as u64 * limits.max_texture_dimension_2d as u64 * 4
+    } else {
+        0
+    }
+}
+
 pub fn hardware_get_summary() -> HardwareSummary {
     let instance = wgpu::Instance::default();
     let adapters = instance.enumerate_adapters(wgpu::Backends::all());
-    let mut discrete_gpu = false;
-    for (_, adapter) in adapters.iter().enumerate() {
-        if adapter.get_info().device_type == DeviceType::DiscreteGpu {
-            discrete_gpu = true;
-            break;
-        }
-    }
+    let adapter = adapters.iter().find(|adapter| adapter.get_info().device_type == wgpu::DeviceType::DiscreteGpu)
+        .unwrap_or_else(|| adapters.first().expect("no gpu adapter found"));
+
+    let discrete_gpu = adapter.get_info().device_type == DeviceType::DiscreteGpu;
+    let is_macos = cfg!(target_os = "macos");
+
     let mut sys = System::new_all();
     sys.refresh_all();
 
-    // memory in GB
-    let memory = sys.total_memory() / 1024 / 1024 / 1024;
+    // v memory in GB
+    let memory = get_total_vram(adapter);
     let cpus = sys.cpus().len();
 
     let requirement_status;
-    if cpus >= RECOMMENDED_CPUS && memory >= RECOMMENDED_MEMORY && discrete_gpu {
+    if cpus >= RECOMMENDED_CPUS && memory >= RECOMMENDED_MEMORY && (discrete_gpu || is_macos) {
         requirement_status = RequirementsStatus::Optimal;
     } else if cpus >= RECOMMENDED_CPUS && memory >= RECOMMENDED_MEMORY {
         requirement_status = RequirementsStatus::Recommended;
@@ -98,6 +115,7 @@ pub fn hardware_get_summary() -> HardwareSummary {
     } else {
         requirement_status = RequirementsStatus::Unmeet;
     }
+
     HardwareSummary {
         hardware: Hardware {
             cpus,
