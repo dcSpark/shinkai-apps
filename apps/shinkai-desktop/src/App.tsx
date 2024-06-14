1a30eb415055d08c61fbde1c1a6883283a04f233
paulclindo <paulclindo@gmail.com>
2024-06-13 20:55:09 -0500

    feat: error boundary

--- apps/shinkai-desktop/src/App.tsx
+++ apps/shinkai-desktop/src/App.tsx
@@ -1,7 +1,9 @@
 import { QueryProvider } from '@shinkai_network/shinkai-node-state';
 import { Toaster } from '@shinkai_network/shinkai-ui';
+import { ErrorBoundary } from 'react-error-boundary';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import FullPageErrorFallback from './components/error-boundary';
 import { AnalyticsProvider } from './lib/posthog-provider';
 import AppRoutes from './routes';
 import { initSyncStorage } from './store/sync-utils';
@@ -12,9 +14,11 @@ function App() {
   return (
     <AnalyticsProvider>
       <QueryProvider>
-        <Router>
-          <AppRoutes />
-        </Router>
+        <ErrorBoundary FallbackComponent={FullPageErrorFallback}>
+          <Router>
+            <AppRoutes />
+          </Router>
+        </ErrorBoundary>
         <Toaster />
       </QueryProvider>
     </AnalyticsProvider>
