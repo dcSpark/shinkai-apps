6b7cc51c5dd3468d0d22b74b793972a1819e173a
Alfredo Gallardo <agallardol@outlook.com>
2024-07-19 16:20:18 -0400

    - fix: download initial embedding model

--- apps/shinkai-desktop/src-tauri/src/local_shinkai_node/shinkai_node_manager.rs
+++ apps/shinkai-desktop/src-tauri/src/local_shinkai_node/shinkai_node_manager.rs
@@ -97,7 +97,7 @@ impl ShinkaiNodeManager {
             return Err(e.to_string());
         }
         let installed_models: Vec<String> = installed_models_response.unwrap().models.iter().map(|m| m.model.clone()).collect();
-        let default_embedding_model = ShinkaiNodeOptions::default().default_embedding_model.unwrap();
+        let default_embedding_model = self.shinkai_node_process.get_options().default_embedding_model.unwrap();
         if !installed_models.contains(&default_embedding_model.to_string()) {
             self.emit_event(ShinkaiNodeManagerEvent::PullingModelStart {
                 model: default_embedding_model.to_string(),
