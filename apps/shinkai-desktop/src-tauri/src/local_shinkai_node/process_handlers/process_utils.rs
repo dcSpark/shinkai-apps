f66d6be0a48cf831c74f18a872308e6f95a2abb8
Alfredo Gallardo <agallardol@outlook.com>
2024-08-05 15:42:02 -0400

    - fix: add env just if has value

--- apps/shinkai-desktop/src-tauri/src/local_shinkai_node/process_handlers/process_utils.rs
+++ apps/shinkai-desktop/src-tauri/src/local_shinkai_node/process_handlers/process_utils.rs
@@ -8,8 +8,9 @@ pub fn options_to_env<T: serde::Serialize>(options: &T) -> HashMap<String, Strin
     let options_reflection = serde_json::to_value(options).unwrap();
     for (key, value) in options_reflection.as_object().unwrap() {
         let env_key = key.to_uppercase();
-        let env_value = value.as_str().unwrap_or_default().to_string();
-        env.insert(env_key, env_value);
+        if let Some(env_value) = value.as_str() {
+            env.insert(env_key, env_value.to_string());
+        }
     }
     env
 }
