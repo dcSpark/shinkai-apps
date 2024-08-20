44ee60b048e53368475e8a702c93e88702bc2816
paulclindo <paulclindo@gmail.com>
2024-08-20 11:31:07 -0500

    update tools UI

--- apps/shinkai-desktop/src/pages/tools.tsx
+++ apps/shinkai-desktop/src/pages/tools.tsx
@@ -96,7 +96,7 @@ export const Tools = () => {
             </Button>
           )}
         </div>
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 gap-4">
           {(isPending || !isSearchQuerySynced || isSearchToolListPending) &&
             Array.from({ length: 8 }).map((_, idx) => (
               <div
