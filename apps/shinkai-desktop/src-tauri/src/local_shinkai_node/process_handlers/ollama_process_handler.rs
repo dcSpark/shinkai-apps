8cc38b7158d6f8be3bd757ab34fcdadb713d371d
Alfredo Gallardo <agallardol@outlook.com>
2024-09-11 11:24:15 -0300

    - feature: enable local pdf parsing (#427)
    * - feature: enable local pdf parsing
    
    * - fix: security issue
    
    * - fix: empty env variables

--- apps/shinkai-desktop/src-tauri/src/local_shinkai_node/process_handlers/ollama_process_handler.rs
+++ apps/shinkai-desktop/src-tauri/src/local_shinkai_node/process_handlers/ollama_process_handler.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 use serde::Serialize;
 use tokio::sync::mpsc::Sender;
 
-use crate::{app::APP_HANDLE, local_shinkai_node::ollama_api::ollama_api_client::OllamaApiClient};
+use crate::local_shinkai_node::ollama_api::ollama_api_client::OllamaApiClient;
 
 use super::{
     logger::LogEntry,
@@ -22,8 +22,45 @@ pub struct OllamaOptions {
     pub ollama_runners_dir: Option<String>,
 }
 
+impl Default for OllamaOptions {
+    fn default() -> Self {
+        Self {
+            ollama_host: "127.0.0.1:11435".to_string(),
+            ollama_num_parallel: "1".to_string(),
+            ollama_max_loaded_models: "2".to_string(),
+            ollama_origins: "*".to_string(),
+            ollama_debug: "true".to_string(),
+            ollama_runners_dir: None,
+        }
+    }
+}
+
+impl OllamaOptions {
+    pub fn with_app_options(app_resource_dir: Option<PathBuf>) -> Self {
+        let ollama_runners_dir = if cfg!(target_os = "windows") {
+            Some(
+                app_resource_dir
+                    .clone()
+                    .map(|dir| {
+                        dir.join("external-binaries/ollama/lib/ollama/runners")
+                            .to_string_lossy()
+                            .to_string()
+                    })
+                    .unwrap_or_default(),
+            )
+        } else {
+            None
+        };
+        Self {
+            ollama_runners_dir,
+            ..Default::default()
+        }
+    }
+}
+
 pub struct OllamaProcessHandler {
     process_handler: ProcessHandler,
+    app_resource_dir: Option<PathBuf>,
     options: OllamaOptions,
 }
 
@@ -32,25 +69,19 @@ impl OllamaProcessHandler {
     const PROCESS_NAME: &'static str = "ollama";
     const READY_MATCHER: &'static str = "Listening on ";
 
-    pub fn new(options: Option<OllamaOptions>, event_sender: Sender<ProcessHandlerEvent>) -> Self {
+    pub fn new(
+        event_sender: Sender<ProcessHandlerEvent>,
+        app_resource_dir: Option<PathBuf>,
+    ) -> Self {
         let ready_matcher = Regex::new(Self::READY_MATCHER).unwrap();
         Self::kill_llama_process();
         let process_handler =
             ProcessHandler::new(Self::PROCESS_NAME.to_string(), event_sender, ready_matcher);
+        let options = OllamaOptions::with_app_options(app_resource_dir.clone());
         OllamaProcessHandler {
             process_handler,
-            options: options.unwrap_or(Self::default_options()),
-        }
-    }
-
-    pub fn default_options() -> OllamaOptions {
-        OllamaOptions {
-            ollama_host: "127.0.0.1:11435".to_string(),
-            ollama_num_parallel: "1".to_string(),
-            ollama_max_loaded_models: "2".to_string(),
-            ollama_origins: "*".to_string(),
-            ollama_debug: "true".to_string(),
-            ollama_runners_dir: None,
+            app_resource_dir,
+            options,
         }
     }
 
@@ -86,52 +117,23 @@ impl OllamaProcessHandler {
     }
 
     pub async fn spawn(&self, ensure_model: Option<&str>) -> Result<(), String> {
-        let resource_dir = APP_HANDLE
-        .get()
-        .unwrap()
-        .lock().await
-        .path_resolver()
-        .resource_dir();
-
         let ollama_process_cwd = if cfg!(target_os = "windows") {
-            Some(
-                PathBuf::from(resource_dir
+            Some(PathBuf::from(
+                self.app_resource_dir
                     .clone()
                     .map(|dir| {
                         dir.join("external-binaries/ollama")
                             .to_string_lossy()
                             .to_string()
                     })
-                    .unwrap_or_default())
-            )
-        } else {
-            None
-        };
-        let ollama_runners_dir = if cfg!(target_os = "windows") {
-            Some(
-                resource_dir
-                    .clone()
-                    .map(|dir| {
-                        dir.join("external-binaries/ollama/lib/ollama/runners")
-                            .to_string_lossy()
-                            .to_string()
-                    })
                     .unwrap_or_default(),
-            )
+            ))
         } else {
             None
         };
-        let options = OllamaOptions {
-            ollama_runners_dir,
-            ..self.options.clone()
-        };
-        let env = options_to_env(&options);
+        let env = options_to_env(&self.options);
         self.process_handler
-            .spawn(
-                env,
-                ["serve"].to_vec(),
-                ollama_process_cwd,
-            )
+            .spawn(env, ["serve"].to_vec(), ollama_process_cwd)
             .await?;
         if let Err(e) = self.wait_ollama_server().await {
             self.process_handler.kill().await;
