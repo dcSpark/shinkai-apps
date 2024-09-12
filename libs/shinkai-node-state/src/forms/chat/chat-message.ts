d4c0eece38474cfba34bc9f09dadf5d9a2f5a561
paulclindo <paulclindo@gmail.com>
2024-09-11 19:34:46 -0500

    set max 3 images

--- libs/shinkai-node-state/src/forms/chat/chat-message.ts
+++ libs/shinkai-node-state/src/forms/chat/chat-message.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const chatMessageFormSchema = z.object({
   message: z.string().min(1),
-  files: z.array(z.any()).max(20).optional(),
+  files: z.array(z.any()).max(3).optional(),
 });
 
 export type ChatMessageFormSchema = z.infer<typeof chatMessageFormSchema>;
