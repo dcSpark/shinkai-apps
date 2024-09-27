b516b277b0aec2ecfbc51c1a0929b7767c832f08
Alfredo Gallardo <agallardol@outlook.com>
2024-09-27 14:45:56 -0300

    - refactor: windows management

--- apps/shinkai-desktop/src-tauri/src/global_shortcuts/toggle_spotlight.rs
+++ apps/shinkai-desktop/src-tauri/src/global_shortcuts/toggle_spotlight.rs
@@ -1,17 +1,14 @@
 use tauri::Manager;
 use tauri_plugin_global_shortcut::{Shortcut, ShortcutEvent};
 
+use crate::windows::{show_or_recreate_window, Window};
+
 pub fn toggle_spotlight(app: &tauri::AppHandle, _: Shortcut, _: ShortcutEvent) {
-    if let Some(window) = app.get_webview_window("spotlight") {
-        if let (Ok(visible), Ok(focused)) = (window.is_visible(), window.is_focused()) {
-            if visible && focused {
-                let _ = window.hide();
-            } else {
-                let _ = window.show();
-                let _ = window.set_focus();
-            }
-        } else {
-            log::error!("failed to get spotlight window visibility");
+    if let Some(spotlight_window) = app.get_webview_window(Window::Spotlight.as_str()) {
+        if spotlight_window.is_visible().unwrap_or(false) && spotlight_window.is_focused().unwrap_or(false) {
+            let _ = spotlight_window.hide();
+            return;
         }
     }
+    show_or_recreate_window(app.clone(), Window::Spotlight)
 }
