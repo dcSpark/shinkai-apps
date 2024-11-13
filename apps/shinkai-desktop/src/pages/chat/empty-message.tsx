510bda8e3f8d07164117ee486c6d7803356c1e72
paulclindo <paulclindo@gmail.com>
2024-11-13 13:30:41 -0500

    fixes

--- apps/shinkai-desktop/src/pages/chat/empty-message.tsx
+++ apps/shinkai-desktop/src/pages/chat/empty-message.tsx
@@ -1,11 +1,15 @@
 import { useTranslation } from '@shinkai_network/shinkai-i18n';
+import { buildInboxIdFromJobId } from '@shinkai_network/shinkai-message-ts/utils/inbox_name_handler';
+import { DEFAULT_CHAT_CONFIG } from '@shinkai_network/shinkai-node-state/v2/constants';
+import { useCreateJob } from '@shinkai_network/shinkai-node-state/v2/mutations/createJob/useCreateJob';
 import { useGetLLMProviders } from '@shinkai_network/shinkai-node-state/v2/queries/getLLMProviders/useGetLLMProviders';
 import { Badge, buttonVariants } from '@shinkai_network/shinkai-ui';
 import { invoke } from '@tauri-apps/api/core';
 import { motion } from 'framer-motion';
 import { ArrowUpRight } from 'lucide-react';
 import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 import { useSetJobScope } from '../../components/chat/context/set-job-scope-context';
 import ConversationFooter from '../../components/chat/conversation-footer';
@@ -13,6 +17,7 @@ import ConversationHeader from '../../components/chat/conversation-header';
 import { usePromptSelectionStore } from '../../components/prompt/context/prompt-selection-context';
 import { useWorkflowSelectionStore } from '../../components/workflow/context/workflow-selection-context';
 import { useAuth } from '../../store/auth';
+import { useSettings } from '../../store/settings';
 import { useShinkaiNodeManager } from '../../store/shinkai-node-manager';
 
 export const showSpotlightWindow = async () => {
@@ -38,12 +43,44 @@ const EmptyMessage = () => {
   const setPromptSelected = usePromptSelectionStore(
     (state) => state.setPromptSelected,
   );
+  const navigate = useNavigate();
+  const defaultAgentId = useSettings((state) => state.defaultAgentId);
 
+  const { mutateAsync: createJob } = useCreateJob({
+    onError: (error) => {
+      toast.error('Failed to send message', {
+        description: error.response?.data?.message ?? error.message,
+      });
+    },
+    onSuccess: async (data) => {
+      navigate(
+        `/inboxes/${encodeURIComponent(buildInboxIdFromJobId(data.jobId))}`,
+      );
+    },
+  });
   useEffect(() => {
     resetJobScope();
     setWorkflowSelected(undefined);
     setPromptSelected(undefined);
   }, []);
+
+  const onCreateJob = async (message: string, isToolNeeded = false) => {
+    if (!auth) return;
+    await createJob({
+      nodeAddress: auth.node_address,
+      token: auth.api_v2_key,
+      llmProvider: defaultAgentId,
+      content: message,
+      isHidden: false,
+      chatConfig: {
+        stream: isToolNeeded ? false : DEFAULT_CHAT_CONFIG.stream,
+        custom_prompt: '',
+        temperature: DEFAULT_CHAT_CONFIG.temperature,
+        top_p: DEFAULT_CHAT_CONFIG.top_p,
+        top_k: DEFAULT_CHAT_CONFIG.top_k,
+      },
+    });
+  };
 
   return (
     <div className="flex max-h-screen flex-1 flex-col overflow-hidden pt-2">
@@ -88,10 +125,6 @@ const EmptyMessage = () => {
                 prompt: 'Summarize a Youtube video: ',
                 isToolNeeded: true,
               },
-              {
-                text: 'Tell me about the Roman Empire',
-                prompt: 'Tell me about the Roman Empire',
-              },
             ].map((suggestion) => (
               <Badge
                 className="cursor-pointer justify-between bg-gray-300 py-2 text-left font-normal normal-case text-gray-50 transition-colors hover:bg-gray-200"
@@ -119,6 +152,14 @@ const EmptyMessage = () => {
                 <ArrowUpRight className="ml-2 h-3.5 w-3.5" />
               </Badge>
             ))}
+            <Badge
+              className="cursor-pointer justify-between text-balance bg-gray-300 py-2 text-left font-normal normal-case text-gray-50 transition-colors hover:bg-gray-200"
+              onClick={() => onCreateJob('Tell me about the Roman Empire')}
+              variant="outline"
+            >
+              Tell me about the Roman Empire
+              <ArrowUpRight className="ml-2 h-3.5 w-3.5" />
+            </Badge>
           </div>
 
           <div className="mt-4">
