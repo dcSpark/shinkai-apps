55833bcce0d0032573a27eb594eb22e932adef01
paulclindo <paulclindo@gmail.com>
2024-11-14 23:17:02 -0500

    feat: agents as experimental feature

--- apps/shinkai-desktop/src/pages/ais.tsx
+++ apps/shinkai-desktop/src/pages/ais.tsx
@@ -47,6 +47,7 @@ import { toast } from 'sonner';
 import Agents from '../components/agent/agents';
 import { useURLQueryParams } from '../hooks/use-url-query-params';
 import { useAuth } from '../store/auth';
+import { useSettings } from '../store/settings';
 import { useShinkaiNodeManager } from '../store/shinkai-node-manager';
 import { getModelObject } from './add-ai';
 import { SimpleLayout } from './layout/simple-layout';
@@ -59,6 +60,8 @@ const AIsPage = () => {
     nodeAddress: auth?.node_address ?? '',
     token: auth?.api_v2_key ?? '',
   });
+  const optInExperimental = useSettings((state) => state.optInExperimental);
+
   const isLocalShinkaiNodeIsUse = useShinkaiNodeManager(
     (state) => state.isInUse,
   );
@@ -79,19 +82,29 @@ const AIsPage = () => {
         className="relative flex h-full flex-col"
         defaultValue={tabSelected}
       >
-        <TabsList className="grid w-full max-w-[200px] grid-cols-2 overflow-auto rounded-lg border border-gray-400 bg-transparent p-0.5">
+        <TabsList
+          className={cn(
+            'grid w-full overflow-auto rounded-lg border border-gray-400 bg-transparent p-0.5',
+
+            optInExperimental
+              ? 'max-w-[200px] grid-cols-2'
+              : 'max-w-[100px] grid-cols-1',
+          )}
+        >
           <TabsTrigger
             className="flex h-8 items-center gap-1.5 text-xs font-semibold"
             value="ais"
           >
             AIs
           </TabsTrigger>
-          <TabsTrigger
-            className="flex h-8 items-center gap-1.5 text-xs font-semibold"
-            value="agents"
-          >
-            Agents
-          </TabsTrigger>
+          {optInExperimental && (
+            <TabsTrigger
+              className="flex h-8 items-center gap-1.5 text-xs font-semibold"
+              value="agents"
+            >
+              Agents
+            </TabsTrigger>
+          )}
         </TabsList>
         <TabsContent className="h-full" value="ais">
           <div className="absolute right-3 top-0">
@@ -140,9 +153,11 @@ const AIsPage = () => {
             )}
           </div>
         </TabsContent>
-        <TabsContent className="h-full" value="agents">
-          <Agents />
-        </TabsContent>
+        {optInExperimental && (
+          <TabsContent className="h-full" value="agents">
+            <Agents />
+          </TabsContent>
+        )}
       </Tabs>
     </SimpleLayout>
   );
