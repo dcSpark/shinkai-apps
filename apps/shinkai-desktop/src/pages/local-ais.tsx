957fa4e15890b86c47ec038b619aaf73bee035a0
paulclindo <paulclindo@gmail.com>
2024-11-29 13:51:33 -0500

    ui updates

--- apps/shinkai-desktop/src/pages/local-ais.tsx
+++ apps/shinkai-desktop/src/pages/local-ais.tsx
@@ -28,23 +28,11 @@ const LocalAisPage = () => {
           parentShowAllOllamaModels={showAllOllamaModels}
         />
         <div className="absolute right-4 top-6 flex justify-center gap-3 pt-3">
-          <Button
-            className={cn('gap-2')}
-            onClick={() => setShowAllOllamaModels(!showAllOllamaModels)}
-            size="auto"
-            variant="outline"
-          >
-            <Sparkles className="h-4 w-4" />
-            <span className="capitalize">
-              {showAllOllamaModels
-                ? t('shinkaiNode.models.labels.showRecommended')
-                : t('shinkaiNode.models.labels.showAll')}
-            </span>
-          </Button>
           <Link
             className={cn(
               buttonVariants({
                 size: 'auto',
+                variant: 'outline',
               }),
               'min-w-[120px] gap-2 py-2.5',
             )}
@@ -53,6 +41,18 @@ const LocalAisPage = () => {
             <Plus className="h-4 w-4" />
             {t('llmProviders.addManually')}
           </Link>
+          <Button
+            className={cn('gap-2')}
+            onClick={() => setShowAllOllamaModels(!showAllOllamaModels)}
+            size="auto"
+          >
+            <Sparkles className="h-4 w-4" />
+            <span className="capitalize">
+              {showAllOllamaModels
+                ? t('shinkaiNode.models.labels.showRecommended')
+                : t('shinkaiNode.models.labels.showAll')}
+            </span>
+          </Button>
         </div>
       </SubpageLayout>
     </QueryClientProvider>
