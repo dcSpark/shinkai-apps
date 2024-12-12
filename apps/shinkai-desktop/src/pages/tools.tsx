8e12eb7cbb69119a7c6c888f4dc7fa4fb84d8cd3
paulclindo <paulclindo@gmail.com>
2024-12-12 17:45:57 -0500

    fixes

--- apps/shinkai-desktop/src/pages/tools.tsx
+++ apps/shinkai-desktop/src/pages/tools.tsx
@@ -17,7 +17,6 @@ import {
   Form,
   FormField,
   Input,
-  ScrollArea,
   Switch,
   TextField,
   Tooltip,
@@ -92,191 +91,187 @@ export const Tools = () => {
       }
       title={t('tools.label')}
     >
-      <ScrollArea className="pr-4 [&>div>div]:!block">
-        <div className="relative mb-4 flex h-10 w-full items-center">
-          <Input
-            className="placeholder-gray-80 !h-full border-none bg-gray-400 py-2 pl-10"
-            onChange={(e) => {
-              setSearchQuery(e.target.value);
+      <div className="relative mb-4 flex h-10 w-full items-center">
+        <Input
+          className="placeholder-gray-80 !h-full border-none bg-gray-400 py-2 pl-10"
+          onChange={(e) => {
+            setSearchQuery(e.target.value);
+          }}
+          placeholder="Search..."
+          spellCheck={false}
+          value={searchQuery}
+        />
+        <SearchIcon className="absolute left-4 top-1/2 -z-[1px] h-4 w-4 -translate-y-1/2" />
+        {searchQuery && (
+          <Button
+            className="absolute right-1 h-8 w-8 bg-gray-200 p-2"
+            onClick={() => {
+              setSearchQuery('');
             }}
-            placeholder="Search..."
-            spellCheck={false}
-            value={searchQuery}
-          />
-          <SearchIcon className="absolute left-4 top-1/2 -z-[1px] h-4 w-4 -translate-y-1/2" />
-          {searchQuery && (
-            <Button
-              className="absolute right-1 h-8 w-8 bg-gray-200 p-2"
-              onClick={() => {
-                setSearchQuery('');
-              }}
-              size="auto"
-              type="button"
-              variant="ghost"
+            size="auto"
+            type="button"
+            variant="ghost"
+          >
+            <XIcon />
+            <span className="sr-only">{t('common.clearSearch')}</span>
+          </Button>
+        )}
+      </div>
+      <div className="divide-y divide-gray-300">
+        {(isPending || !isSearchQuerySynced || isSearchToolListPending) &&
+          Array.from({ length: 8 }).map((_, idx) => (
+            <div
+              className={cn(
+                'grid animate-pulse grid-cols-[1fr_115px_36px] items-center gap-5 rounded-sm bg-gray-500 px-2 py-4 text-left text-sm',
+              )}
+              key={idx}
             >
-              <XIcon />
-              <span className="sr-only">{t('common.clearSearch')}</span>
-            </Button>
-          )}
-        </div>
-        <div className="divide-y divide-gray-300">
-          {(isPending || !isSearchQuerySynced || isSearchToolListPending) &&
-            Array.from({ length: 8 }).map((_, idx) => (
-              <div
-                className={cn(
-                  'grid animate-pulse grid-cols-[1fr_115px_36px] items-center gap-5 rounded-sm bg-gray-500 px-2 py-4 text-left text-sm',
-                )}
-                key={idx}
-              >
-                <div className="flex w-full flex-1 flex-col gap-3">
-                  <span className="h-4 w-36 rounded-sm bg-gray-300" />
-                  <div className="flex flex-col gap-1">
-                    <span className="h-3 w-full rounded-sm bg-gray-300" />
-                    <span className="h-3 w-2/4 rounded-sm bg-gray-300" />
-                  </div>
+              <div className="flex w-full flex-1 flex-col gap-3">
+                <span className="h-4 w-36 rounded-sm bg-gray-300" />
+                <div className="flex flex-col gap-1">
+                  <span className="h-3 w-full rounded-sm bg-gray-300" />
+                  <span className="h-3 w-2/4 rounded-sm bg-gray-300" />
                 </div>
-                <span className="h-7 w-full rounded-md bg-gray-300" />
-                <span className="h-5 w-[36px] rounded-full bg-gray-300" />
               </div>
-            ))}
-          {!searchQuery &&
-            isSearchQuerySynced &&
-            toolsList?.map((tool) => (
-              <div
-                className={cn(
-                  'grid grid-cols-[1fr_115px_36px] items-center gap-5 rounded-sm bg-gray-500 px-2 py-4 text-left text-sm',
-                )}
-                key={tool.tool_router_key}
-              >
-                <div className="flex flex-col gap-2.5">
-                  <div className="flex items-center gap-2">
-                    <span className="text-sm font-medium capitalize text-white">
-                      {formatText(tool.name)}{' '}
-                    </span>
-                    {tool.author !== '@@official.shinkai' && (
-                      <Badge className="text-gray-80 bg-gray-200 text-xs font-normal">
-                        {tool.author}
-                      </Badge>
-                    )}
-                  </div>
-                  <p className="text-gray-80 line-clamp-2 text-xs">
-                    {tool.description}
-                  </p>
-                </div>
-                <Link
-                  className={cn(
-                    buttonVariants({
-                      variant: 'outline',
-                      size: 'sm',
-                    }),
-                    'min-h-auto h-auto rounded-md py-2',
+              <span className="h-7 w-full rounded-md bg-gray-300" />
+              <span className="h-5 w-[36px] rounded-full bg-gray-300" />
+            </div>
+          ))}
+        {!searchQuery &&
+          isSearchQuerySynced &&
+          toolsList?.map((tool) => (
+            <div
+              className={cn(
+                'grid grid-cols-[1fr_115px_36px] items-center gap-5 rounded-sm bg-gray-500 px-2 py-4 text-left text-sm',
+              )}
+              key={tool.tool_router_key}
+            >
+              <div className="flex flex-col gap-2.5">
+                <div className="flex items-center gap-2">
+                  <span className="text-sm font-medium capitalize text-white">
+                    {formatText(tool.name)}{' '}
+                  </span>
+                  {tool.author !== '@@official.shinkai' && (
+                    <Badge className="text-gray-80 bg-gray-200 text-xs font-normal">
+                      {tool.author}
+                    </Badge>
                   )}
-                  to={`/tools/${tool.tool_router_key}`}
-                >
-                  <BoltIcon className="mr-1.5 h-4 w-4" />
-                  {t('common.configure')}
-                </Link>
-                <TooltipProvider delayDuration={0}>
-                  <Tooltip>
-                    <TooltipTrigger className="flex items-center gap-1">
-                      <Switch
-                        checked={tool.enabled}
-                        onCheckedChange={async () => {
-                          await updateTool({
-                            toolKey: tool.tool_router_key,
-                            toolType: tool.tool_type,
-                            toolPayload: {} as ShinkaiTool,
-                            isToolEnabled: !tool.enabled,
-                            nodeAddress: auth?.node_address ?? '',
-                            token: auth?.api_v2_key ?? '',
-                          });
-                        }}
-                      />
-                    </TooltipTrigger>
-                    <TooltipPortal>
-                      <TooltipContent align="center" side="top">
-                        {t('common.enabled')}
-                      </TooltipContent>
-                    </TooltipPortal>
-                  </Tooltip>
-                </TooltipProvider>
-              </div>
-            ))}
-          {searchQuery &&
-            isSearchQuerySynced &&
-            searchToolList?.map((tool) => (
-              <div
-                className={cn(
-                  'grid grid-cols-[1fr_115px_36px] items-center gap-5 rounded-sm bg-gray-500 px-2 py-4 text-left text-sm',
-                )}
-                key={tool.tool_router_key}
-              >
-                <div className="flex flex-col gap-2.5">
-                  <div className="flex items-center gap-2">
-                    <span className="text-sm font-medium text-white">
-                      {formatText(tool.name)}{' '}
-                    </span>
-                    {tool.author !== '@@official.shinkai' && (
-                      <Badge className="text-gray-80 bg-gray-200 text-xs font-normal">
-                        {tool.author}
-                      </Badge>
-                    )}
-                  </div>
-                  <p className="text-gray-80 line-clamp-2 text-xs">
-                    {tool.description}
-                  </p>
                 </div>
-
-                <Link
-                  className={cn(
-                    buttonVariants({
-                      variant: 'outline',
-                      size: 'sm',
-                    }),
-                    'min-h-auto h-auto rounded-md py-2',
-                  )}
-                  to={`/tools/${tool.tool_router_key}`}
-                >
-                  <BoltIcon className="mr-1.5 h-4 w-4" />
-                  {t('common.configure')}
-                </Link>
-                <TooltipProvider delayDuration={0}>
-                  <Tooltip>
-                    <TooltipTrigger className="flex items-center gap-1">
-                      <Switch
-                        checked={tool.enabled}
-                        onCheckedChange={async () => {
-                          await updateTool({
-                            toolKey: tool.tool_router_key,
-                            toolType: tool.tool_type,
-                            toolPayload: {} as ShinkaiTool,
-                            isToolEnabled: !tool.enabled,
-                            nodeAddress: auth?.node_address ?? '',
-                            token: auth?.api_v2_key ?? '',
-                          });
-                        }}
-                      />
-                    </TooltipTrigger>
-                    <TooltipPortal>
-                      <TooltipContent align="center" side="top">
-                        {t('common.enabled')}
-                      </TooltipContent>
-                    </TooltipPortal>
-                  </Tooltip>
-                </TooltipProvider>
-              </div>
-            ))}
-          {searchQuery &&
-            isSearchQuerySynced &&
-            searchToolList?.length === 0 && (
-              <div className="flex h-20 items-center justify-center">
-                <p className="text-gray-80 text-sm">
-                  {t('tools.emptyState.search.text')}
+                <p className="text-gray-80 line-clamp-2 text-xs">
+                  {tool.description}
                 </p>
               </div>
-            )}
-        </div>
-      </ScrollArea>
+              <Link
+                className={cn(
+                  buttonVariants({
+                    variant: 'outline',
+                    size: 'sm',
+                  }),
+                  'min-h-auto h-auto rounded-md py-2',
+                )}
+                to={`/tools/${tool.tool_router_key}`}
+              >
+                <BoltIcon className="mr-1.5 h-4 w-4" />
+                {t('common.configure')}
+              </Link>
+              <TooltipProvider delayDuration={0}>
+                <Tooltip>
+                  <TooltipTrigger className="flex items-center gap-1">
+                    <Switch
+                      checked={tool.enabled}
+                      onCheckedChange={async () => {
+                        await updateTool({
+                          toolKey: tool.tool_router_key,
+                          toolType: tool.tool_type,
+                          toolPayload: {} as ShinkaiTool,
+                          isToolEnabled: !tool.enabled,
+                          nodeAddress: auth?.node_address ?? '',
+                          token: auth?.api_v2_key ?? '',
+                        });
+                      }}
+                    />
+                  </TooltipTrigger>
+                  <TooltipPortal>
+                    <TooltipContent align="center" side="top">
+                      {t('common.enabled')}
+                    </TooltipContent>
+                  </TooltipPortal>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+          ))}
+        {searchQuery &&
+          isSearchQuerySynced &&
+          searchToolList?.map((tool) => (
+            <div
+              className={cn(
+                'grid grid-cols-[1fr_115px_36px] items-center gap-5 rounded-sm bg-gray-500 px-2 py-4 text-left text-sm',
+              )}
+              key={tool.tool_router_key}
+            >
+              <div className="flex flex-col gap-2.5">
+                <div className="flex items-center gap-2">
+                  <span className="text-sm font-medium text-white">
+                    {formatText(tool.name)}{' '}
+                  </span>
+                  {tool.author !== '@@official.shinkai' && (
+                    <Badge className="text-gray-80 bg-gray-200 text-xs font-normal">
+                      {tool.author}
+                    </Badge>
+                  )}
+                </div>
+                <p className="text-gray-80 line-clamp-2 text-xs">
+                  {tool.description}
+                </p>
+              </div>
+
+              <Link
+                className={cn(
+                  buttonVariants({
+                    variant: 'outline',
+                    size: 'sm',
+                  }),
+                  'min-h-auto h-auto rounded-md py-2',
+                )}
+                to={`/tools/${tool.tool_router_key}`}
+              >
+                <BoltIcon className="mr-1.5 h-4 w-4" />
+                {t('common.configure')}
+              </Link>
+              <TooltipProvider delayDuration={0}>
+                <Tooltip>
+                  <TooltipTrigger className="flex items-center gap-1">
+                    <Switch
+                      checked={tool.enabled}
+                      onCheckedChange={async () => {
+                        await updateTool({
+                          toolKey: tool.tool_router_key,
+                          toolType: tool.tool_type,
+                          toolPayload: {} as ShinkaiTool,
+                          isToolEnabled: !tool.enabled,
+                          nodeAddress: auth?.node_address ?? '',
+                          token: auth?.api_v2_key ?? '',
+                        });
+                      }}
+                    />
+                  </TooltipTrigger>
+                  <TooltipPortal>
+                    <TooltipContent align="center" side="top">
+                      {t('common.enabled')}
+                    </TooltipContent>
+                  </TooltipPortal>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+          ))}
+        {searchQuery && isSearchQuerySynced && searchToolList?.length === 0 && (
+          <div className="flex h-20 items-center justify-center">
+            <p className="text-gray-80 text-sm">
+              {t('tools.emptyState.search.text')}
+            </p>
+          </div>
+        )}
+      </div>
     </SimpleLayout>
   );
 };
