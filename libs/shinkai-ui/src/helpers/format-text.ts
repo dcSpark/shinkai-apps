c2010a04620216a8165b7c6346125f23e6fa2b65
paulclindo <paulclindo@gmail.com>
2025-01-09 14:04:23 -0500

    fixes and i18n

--- libs/shinkai-ui/src/helpers/format-text.ts
+++ libs/shinkai-ui/src/helpers/format-text.ts
@@ -1,15 +1,12 @@
-import { ShinkaiToolHeader } from "@shinkai_network/shinkai-message-ts/api/tools/types";
+import { ShinkaiToolHeader } from '@shinkai_network/shinkai-message-ts/api/tools/types';
 
 export const formatText = (text: string) => {
-  const words = text.split('_');
-
-  const formattedWords = words.map((word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-  });
-
-  const result = formattedWords.join(' ');
-
-  return result.charAt(0).toUpperCase() + result.slice(1);
+  const camelToSpaces = text.replace(/([a-z])([A-Z])/g, '$1 $2');
+  const snakeToSpaces = camelToSpaces.replace(/_/g, ' ');
+  return snakeToSpaces
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
 };
 
 export const formatCamelCaseText = (text: string) => {
