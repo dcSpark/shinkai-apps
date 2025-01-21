0e7d3ccc8d0d789b12bac3223b2f5f482b1445e8
Guillermo Valin <contacto@guillermovalin.com>
2025-01-21 09:49:23 -0300

    Revert "WIP: Trying to fix crash when app is not open."
    This reverts commit dd5241c0c20d570fd090b5d3a1b7c01a27162667.

--- apps/shinkai-desktop/src-tauri/src/deep_links.rs
+++ apps/shinkai-desktop/src-tauri/src/deep_links.rs
@@ -1,4 +1,4 @@
-use tauri::{window, Emitter, Listener};
+use tauri::Emitter;
 use tauri_plugin_deep_link::DeepLinkExt;
 
 use crate::windows::{recreate_window, Window};
@@ -31,38 +31,31 @@ pub fn setup_deep_links(app: &tauri::AppHandle) -> tauri::Result<()> {
             log::debug!("handling deep link: {:?}", url);
             if let Some(host) = url.host() {
                 if host.to_string() == "store" {
-                    if let Ok(window) = recreate_window(app_handle.clone(), Window::Main, true) {
-                        let app_handle = app_handle.clone();
-                        let url = url.clone();
-                        
-                        window.once("app-ready", move |_event| {
-                            log::debug!(
-                                "emitting store-deep-link event to {}",
-                                Window::Coordinator.as_str()
-                            );
-                            // shinkai://store?type=tool&url=https://download.shinkai.app/tool/email-fetcher.zip
-                            let query_pairs = url.query_pairs().collect::<Vec<_>>();
-                            let tool_type = query_pairs
-                                .iter()
-                                .find(|(key, _)| key == "type")
-                                .map(|(_, value)| value.to_string())
-                                .unwrap_or_default();
-                            let tool_url = query_pairs
-                                .iter()
-                                .find(|(key, _)| key == "url")
-                                .map(|(_, value)| value.to_string())
-                                .unwrap_or_default();
+                    // shinkai://store?type=tool&url=https://download.shinkai.app/tool/email-fetcher.zip
+                    let query_pairs = url.query_pairs().collect::<Vec<_>>();
+                    let tool_type = query_pairs
+                        .iter()
+                        .find(|(key, _)| key == "type")
+                        .map(|(_, value)| value.to_string())
+                        .unwrap_or_default();
+                    let tool_url = query_pairs
+                        .iter()
+                        .find(|(key, _)| key == "url")
+                        .map(|(_, value)| value.to_string())
+                        .unwrap_or_default();
+                    
+                    let payload = StoreDeepLinkPayload { tool_type, tool_url };
 
-                            let window =
-                                recreate_window(app_handle.clone(), Window::Main, true).unwrap();
-                            let payload = StoreDeepLinkPayload {
-                                tool_type,
-                                tool_url,
-                            };
-
-                            let _ = window.emit("store-deep-link", payload);
-                        });
-                    }
+                    log::debug!(
+                        "emitting store-deep-link event to {}",
+                        Window::Coordinator.as_str()
+                    );
+                    let _ = recreate_window(app_handle.clone(), Window::Main, true);
+                    let _ = app_handle.emit_to(
+                        Window::Coordinator.as_str(),
+                        "store-deep-link",
+                        payload,
+                    );
                 }
 
                 if host.to_string() == "oauth" {
--- apps/shinkai-desktop/src/pages/layout/main-layout.tsx
+++ apps/shinkai-desktop/src/pages/layout/main-layout.tsx
@@ -38,7 +38,6 @@ import {
 import { submitRegistrationNoCodeError } from '@shinkai_network/shinkai-ui/helpers';
 import { cn } from '@shinkai_network/shinkai-ui/utils';
 import { listen } from '@tauri-apps/api/event';
-import { getCurrentWindow } from "@tauri-apps/api/window";
 import { AnimatePresence, motion, TargetAndTransition } from 'framer-motion';
 import { ArrowLeftToLine, ArrowRightToLine, BotIcon } from 'lucide-react';
 import React, { Fragment, useEffect, useState } from 'react';
@@ -702,9 +701,6 @@ const MainLayout = () => {
 
   const displaySidebar =
     !!auth && !disabledSidebarRoutes.includes(location.pathname);
-
-  const window = getCurrentWindow();
-  window.emit('app-ready');
 
   return (
     <div className="relative flex h-screen min-h-full flex-col overflow-hidden bg-gray-500 text-white">
