dbe26f3972bfd2760940bc50516c0a0dd8dbc4f7
paulclindo <paulclindo@gmail.com>
2025-01-31 13:36:24 -0500

    feat: auto enable when saving if tool doesnt contain configs

--- libs/shinkai-node-state/src/v2/mutations/saveToolCode/index.ts
+++ libs/shinkai-node-state/src/v2/mutations/saveToolCode/index.ts
@@ -1,4 +1,9 @@
 import { saveToolCode as saveToolCodeApi } from '@shinkai_network/shinkai-message-ts/api/tools/index';
+import { updateTool as updateToolApi } from '@shinkai_network/shinkai-message-ts/api/tools/index';
+import {
+  CodeLanguage,
+  ShinkaiTool,
+} from '@shinkai_network/shinkai-message-ts/api/tools/types';
 import { merge } from 'ts-deepmerge';
 
 import { SaveToolCodeInput } from './types';
@@ -25,7 +30,7 @@ export const saveToolCode = async ({
     tools,
   });
 
-  return await saveToolCodeApi(
+  const response = await saveToolCodeApi(
     nodeAddress,
     token,
     {
@@ -38,4 +43,21 @@ export const saveToolCode = async ({
     xShinkaiAppId,
     xShinkaiToolId,
   );
+
+  if (
+    response.metadata.tool_router_key &&
+    Object.keys(metadata?.configurations?.properties ?? {}).length === 0
+  ) {
+    await updateToolApi(nodeAddress, token, response.metadata.tool_router_key, {
+      content: [{} as ShinkaiTool, true],
+      type:
+        language === CodeLanguage.Typescript
+          ? 'Deno'
+          : language === CodeLanguage.Python
+            ? 'Python'
+            : 'Rust', // TODO: add Rust support
+    });
+  }
+
+  return response;
 };
