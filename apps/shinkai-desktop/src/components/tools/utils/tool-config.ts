9d80b79ef87d97e4aab63f5bee0292828ab6ad41
Nico Arqueros <nico@dcspark.io>
2025-04-01 16:21:49 -0500

    fix config metadata

--- apps/shinkai-desktop/src/components/tools/utils/tool-config.ts
+++ apps/shinkai-desktop/src/components/tools/utils/tool-config.ts
@@ -2,8 +2,19 @@ import { RJSFSchema } from '@rjsf/utils';
 import { ToolConfig } from '@shinkai_network/shinkai-message-ts/api/tools/types';
 import { JSONSchema7TypeName } from 'json-schema';
 
+type ToolConfigSchema = {
+  type: 'object';
+  required: string[];
+  properties: {
+    [key: string]: {
+      type: JSONSchema7TypeName;
+      description: string;
+    };
+  };
+};
+
 export function parseConfigToJsonSchema(config: ToolConfig[]): RJSFSchema {
-  const schema: RJSFSchema = {
+  const schema: ToolConfigSchema = {
     type: 'object',
     required: [],
     properties: {},
@@ -14,12 +25,12 @@ export function parseConfigToJsonSchema(config: ToolConfig[]): RJSFSchema {
     const { key_name, description, required, type } = BasicConfig;
 
     if (required) {
-      schema.required?.push(key_name);
+      schema.required.push(key_name);
     }
 
-    schema.properties![key_name] = {
+    schema.properties[key_name] = {
       type: (type || 'string') as JSONSchema7TypeName,
-      description,
+      description: `${description}${required ? ' (Required)' : ' (Optional)'}`,
     };
   });
 
