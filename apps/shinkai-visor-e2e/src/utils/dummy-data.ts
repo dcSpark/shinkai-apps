3727a32567520b045ad10bbb17de84b1fe08b2b9
Alfredo Gallardo <agallardol@outlook.com>
2025-04-29 15:46:53 -0400

    chore: removed unused projects (#836)

--- apps/shinkai-visor-e2e/src/utils/dummy-data.ts
+++
@@ -1,9 +0,0 @@
-export const getAgent = () => {
-  return {
-    agentName: `test_gpt4_turbo_${Date.now()}`,
-    externalUrl: 'https://api.openai.com',
-    apiKey: process.env.OPEN_AI_API_KEY,
-    model: 'open-ai',
-    models: 'gpt-4-1106-preview',
-  };
-};
--- apps/shinkai-visor/src/service-worker/service-worker.ts
+++
@@ -1,7 +0,0 @@
-import './context-menu';
-
-import { listen as listenExternal } from './communication/external';
-import { listen as listenInternal } from './communication/internal';
-
-listenInternal();
-listenExternal();
